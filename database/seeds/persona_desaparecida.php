53baf9b853cff3bf0c55a5f2a51f73c98cf387d5
ADFGE\dlanda <proyectosmx94@gmail.com>
2018-03-23 18:38:46 -0600

    xD

---
+++ database/seeds/persona_desaparecida.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class persona_desaparecida extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        db_desaparecidos::table(nombre_desaparecidos)
+    }
+}
