85da29a21e2466171620305bb91270f49b3b6801
ADFGE\dlanda <proyectosmx94@gmail.com>
2018-03-23 19:41:03 -0600

    Otro push

--- app/nombre_desaparecidos.php
+++ app/nombre_desaparecidos.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class nombre_desaparecidos extends Model
 {
-    //
+    protected $table = 'persona_desaparecida';
+    protected $fillable = [
+    	'nombre',
+    	'apellidoPaterno',
+    	'apellidoMaterno',
+    	'apodo',
+    	'edadAparente',
+    	'nacionalidad',
+    	'fechaNacimiento',
+    	'estadoCivil',
+    	'genero',
+    	'embarazo',
+    	'periodoGestacion',
+    	'rumores',
+    	'pormenores',
+    	'escolaridad',
+    	'ocupacion'
+    ];
+
+    	public function parentesco(){
+    		return $this->belongsTo('App\parentesco');
+    	}
+
 }
