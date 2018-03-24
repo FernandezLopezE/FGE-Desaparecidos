3224e9d415577f53c0faf106d90b60ac5fa26ffe
ADFGE\AlejandroFernandezToledo <afernandezt@fiscaliaveracruz.gob.mx>
2018-03-23 19:43:10 -0600

    cambio en nombre_desaparecidos

--- app/nombre_desaparecidos.php
+++ app/nombre_desaparecidos.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class nombre_desaparecidos extends Model
 {
     //
+
+
+
+
+
+public function domiciliosDesaparecidos()
+
+	{
+		return $this->hasMany('App\nombre_desaparecidos','idPersona','id');
+	}
+	
 }
