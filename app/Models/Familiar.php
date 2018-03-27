f74471cc00b29893baeed25e19cc3146934c9071
Carlos Avila Aguilar <cavila@fiscaliaveracruz.gob.mx>
2018-03-26 22:18:19 -0600

    Actualizando los modulos

---
+++ app/Models/Familiar.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Familiar extends Model
+{
+	protected $table = 'desaparecidos_familiares';
+	protected $fillable = [
+			'parentesco',
+			'nombres',
+			'primerAp',
+			'segundoAp',
+			'edad',
+			'idDesaparecido',
+	];
+
+	public function desaparecido()
+	{
+		return $this->belongsTo('App\Models\Desaparecido','id');
+	}
+
+}
+  
