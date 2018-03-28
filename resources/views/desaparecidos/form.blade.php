e24dbd936cea7a6e934e9d63f00391e11e90bc70
Carlos Avila Aguilar <cavila@fiscaliaveracruz.gob.mx>
2018-03-28 17:16:14 -0600

    Agregando el form para enviar los datos al controlador.

--- resources/views/desaparecidos/form.blade.php
+++ resources/views/desaparecidos/form.blade.php
@@ -8,12 +8,24 @@
 @endsection
 
 @section('content')
+	{!! Form::model($desaparecido, ['action' => 'DesaparecidoController@store']) !!}
+			@if(count($errors) > 0)
+				<div class="alert alert-danger">
+					<ul>
+						@foreach($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				</div>
+			@endif
 
-	@include('desaparecidos.datosgral2')
-	@include('desaparecidos.domicilio')
+		@include('desaparecidos.datosgral2')
+		@include('desaparecidos.domicilio')
 
-	@include('desaparecidos.datosidentidad')
-	@include('desaparecidos.nacionalidad')
+		@include('desaparecidos.datosidentidad')
+		@include('desaparecidos.nacionalidad')
+		{!! Form::submit('Agregar datos'); !!}
+	{!! Form::close() !!}
 
 @endsection
 
