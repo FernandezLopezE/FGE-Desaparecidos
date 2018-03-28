ee46ace49547fab8e6aff5b127a16588c891b7f0
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-03-27 18:06:31 -0600

    creando request desaparecidos

--- app/Http/Requests/DesaparecidoRequest.php
+++ app/Http/Requests/DesaparecidoRequest.php
@@ -24,8 +24,42 @@ class DesaparecidoRequest extends FormRequest
     public function rules()
     {
         return [
-            //
-            
+            //Datos Persona
+            'nombres' => 'required',
+            'primerAp' => 'required',
+            'segundoAp' => 'required',
+            'fechaNacimiento' => 'required',
+            'idNacionalidad' => 'required'
+            'sexo' => 'required',
+
+            //Datos familia
+            'parentesco' => 'required',
+            'nombres' => 'required',
+            'primerAp' => 'required',
+            'segundoAp' => 'required',
+            'edad' => 'required',
+            'idDesaparecido' => 'required',
+
+            //Datos Domicilio
+            'tipoDireccion' => 'required',
+            'calle' => 'required',
+            'numExterno' => 'required',
+            'numInterno' => 'required',
+            'telefono' => 'required',
+            'idMunicipio' => 'required',
+
+            //Datos desaparecido
+            'apodo' => 'required',
+            'edadAparente' => 'required',
+            'embarazo' => 'required',
+            'periodo' => 'required',
+            'pormenores' => 'required',
+            'antecedentesJudiciales' => 'required',
+            'idEdoCivil' => 'required',
+            'idOcupacion' => 'requied',
+            'idEscolaridad' => 'required',
+            'idPersona' => 'required',
+
             'identificacion' => 'required',
             'otroId' => 'required',
             'noId' => 'required',
