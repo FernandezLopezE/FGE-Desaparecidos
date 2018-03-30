6e9338be8d7bfb22980e7c906db3fc8351ae4c95
Berenice Contreras Dominguez <bnicecd@gmail.com>
2018-03-29 19:49:14 -0600

    Para validar los formularios se agrego $.validate();

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -8,6 +8,9 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <script src="personal/js/select2.min.js"></script>
+        <script type="text/javascript">
+			$.validate();
+		</script>
 		{!! Html::style('bootstrap4/css/bootstrap.css') !!}
 		<!-- icons-fontawesome -->
 	 
