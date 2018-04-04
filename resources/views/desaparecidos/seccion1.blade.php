7f897268dd54fd7b9f0164158ec8127982c48eb0
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-04-03 22:34:56 -0500

    Estandarisando nomenclatura de la estructura de las vistas dividas por secciones

---
+++ resources/views/desaparecidos/seccion1.blade.php
@@ -0,0 +1,3 @@
+@include('desaparecidos.secciones.seccion1_persona_autorizada')
+@include('desaparecidos.secciones.seccion1_persona_entrevistador')
+@include('desaparecidos.secciones.seccion1_persona_informante')
