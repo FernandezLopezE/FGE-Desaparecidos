0a886f3a9bee015e6594e9f438a2aad937fea699
ADFGE\dlanda <proyectosmx94@gmail.com>
2018-04-04 19:29:42 -0500

    Formulario de "Datos de los informantes"

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,5 +1,6 @@
-@extends('layouts.app2')
-
+{!! Form::label ('','Tipo de persona:') !!}
+				{!! Form::checkbox('workday[]', 'monday') !!}
+			</div>
 @section('content')
 	<h1>Hola, Este es el diseño de la plantilla base.</h1>
 
