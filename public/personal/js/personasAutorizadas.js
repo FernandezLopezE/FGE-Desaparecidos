ed844fe8409bea35ff49f55b96a2f82a551dae98
ADFGE\dlanda <proyectosmx94@gmail.com>
2018-04-06 17:18:56 -0500

    Primeros pasos de ventana modal en form de "Personas autorizadas"

--- public/personal/js/personasAutorizadas.js
+++ public/personal/js/personasAutorizadas.js
@@ -1,4 +1,107 @@
 jQuery(document).ready(function($) {
+	
+	//Form de ventana modal
+		$("#entrevistadorPrimeraVez").change(function(){
+			g = $('#entrevistadorPrimeraVez').val();
+
+			console.log("Hola: "+g);
+
+			//Mostrar-ocultar formulario para agregar hijos
+
+			if (g =='NO'){
+				$("#cuando").show();
+			
+				
+
+			} else {
+				$("#cuando").hide();
+
+			} 
+
+		});
+
+
+	//Mostrar campos al seleccionar otro dialecto
+    $('#entrevistadorIdioma').change(function() {
+        e = $('#entrevistadorIdioma').val();
+
+        if (e=='ESPAÑOL') {
+        	$("#otro_dialec").hide();
+        	$("#otro_dialec2").hide();
+        	$("#otro_dialec3").hide();
+        	$("#otro_dialec4").hide();
+        	$("#otro_dialec5").hide();
+        }else{
+        	$("#otro_dialec").show();
+        	$("#otro_dialec2").show();
+        	$("#otro_dialec3").show();
+        	$("#otro_dialec4").show();
+        	$("#otro_dialec5").show();
+        }
+    });
+
+    $('#entrevistadorIdioma').change(function() {
+        e = $('#entrevistadorIdioma').val();
+        console.log(e);
+
+        if (e=='OTRO') {
+        	$("#otro_dialec").show();
+        }else{
+        	$("#otro_dialec").hide();
+        }
+    });
+    //Fin de Mostrar campos al seleccionar otro dialecto
+
+    //ingresar otro parentesco
+    $('#informanteidParentesco').change(function() {
+    	f = $('#informanteidParentesco').val();
+    	console.log("El parentesoc es: "+f);
+
+    	if (f=='OTRO') {
+    		$("#otro_parent").show();
+    	}else{
+    		$("#otro_parent").hide();
+    	}
+    });
+
+    $('#informanteidDocumentoIdentidad').change(function(){
+    	h = $('#informanteidDocumentoIdentidad').val();
+    	
+    	if (h=='Otro(especifique)') {
+    		$("#otro_doc").show();
+    	}else{
+    		$("#otro_doc").hide();
+    	}
+    });
+
+  
+
+
+    $('#fechaNacimiento').change(function(){
+    	f = $('#fechaNacimiento').val();
+    	console.log('datos fecha: '+f);
+
+    	$.ajax({
+                url: '/desaparecido/edad/'+f,
+                type:"GET",
+                dataType:"json",
+
+                success:function(data) {
+                		console.log("hola"+data);
+                		$('#edadExtravio').val(data);
+                },
+                
+            });
+    	/*$.get('/desaparecido/edad/',{fechaNacimiento: f})
+    	.done(function(data){
+    			console.log(data);
+    	});*/
+    }); 
+
+});
+	//Fin del form de ventana modal
+
+
 	 $("input:checked").val(),
 
 	$('#informante').on('ifChecked', function () {
--- resources/views/desaparecidos/secciones/seccion1_persona_autorizada.blade.php
+++ resources/views/desaparecidos/secciones/seccion1_persona_autorizada.blade.php
@@ -25,14 +25,14 @@
 		      <td>MADRE</td>
 		      <td>2281459373</td>
 		      <td>INFORMANTE / RECIBIR INFORMACIÓN</td>
-		      <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#mostrarModal">MOSTRAR</button></td>
+		      <td><button type="button" class="btn btn-dark" data-toggle="modal" data-target="#mostrarModal">MOSTRAR</button></td>
 		    </tr>
 		  </tbody>
 		</table>
 
 		<!-- Ventana Modal -->
 			<div class="modal fade" id="mostrarModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-			  <div class="modal-dialog" role="document">
+			  <div class="modal-dialog modal-lg" role="document">
 			    <div class="modal-content">
 			      <div class="modal-header">
 			        <h5 class="modal-title" id="exampleModalLabel">DATOS DE LOS INFORMANTES</h5>
@@ -41,11 +41,148 @@
 			        </button>
 			      </div>
 			      <div class="modal-body">
-			        ...
+			        <div class="card border-primary">
+	
+	<div class="card-body">
+		<div class="row" id=""  >
+			<div class="col-lg-3">
+				{!! Form::label ('informanteNombres','Nombre(s):') !!}
+				{!! Form::text ('informanteNombres',old('informanteNombres'), ['class' => 'form-control mayuscula', 'id' => 'informanteNombres', ] )!!}
+			</div>
+			<div class="col-lg-3">
+				{!! Form::label ('informantePrimerAp','Primer apellido:') !!}
+				{!! Form::text ('informantePrimerAp',old('informantePrimerAp'), ['class' => 'form-control mayuscula', 'id' => 'informantePrimerAp', ] )!!}
+		  	</div>
+		  	<div class="col-lg-3">
+				{!! Form::label ('informanteSegundoAp','Segundo apellido:') !!}
+				{!! Form::text ('informanteSegundoAp',old('informanteSegundoAp'), ['class' => 'form-control mayuscula', 'id' => 'informanteSegundoAp', ] )!!}
+		  	</div>
+		  	<div class="col-lg-3">
+			{!! Form::label ('informanteidParentesco','Parentesco:') !!}
+			{!! Form::select ('informanteidParentesco',$parentescos,'', ['class' => 'form-control', 'id' => 'informanteidParentesco'] )!!}		
+		</div>
+		</div>
+
+	<div class="row">
+		<div class="col" id="otro_parent" style="display:none">
+			{!! Form::label ('informanteOtroParentesco','Especifique:') !!}
+			{!! Form::text ('informanteOtroParentesco',old('informanteOtroParentesco'), ['class' => 'form-control mayuscula', 'id' => 'informanteOtroParentesco', ] )!!}
+		</div>
+	</div><br>
+
+	<div class="row" id=""  > 	
+		<div class="col-lg-3">
+			{!! Form::label ('idNacionalidad','Nacionalidad:') !!}
+			{!! Form::select ('idNacionalidad',$nacionalidades,'', ['class' => 'form-control', 'id' => 'idNacionalidad', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Elija una opción'] )!!}				
+		</div>
+		<div class="col-lg-3">
+			{!! Form::label ('informanteidDocumentoIdentidad','Documento con que se identifica:') !!}
+			{!! Form::select ('informanteidDocumentoIdentidad',$identificacion,'', ['class' => 'form-control', 'id' => 'informanteidDocumentoIdentidad'] )!!}
+			
+		</div>
+		<div class="col" id="otro_doc" style="display:none">
+			{!! Form::label ('informanteOtroDocIdentidad','Especifique:') !!}
+			{!! Form::text ('informanteOtroDocIdentidad',old('informanteOtroDocIdentidad'), ['class' => 'form-control mayuscula', 'id' => 'informanteOtroDocIdentidad', ] )!!}				
+		</div>
+		<div class="col">
+			{!! Form::label ('informanteNumDocIdentidad','Número de identificación:') !!}
+			{!! Form::text ('informanteNumDocIdentidad',old('informanteNumDocIdentidad'), ['class' => 'form-control mayuscula', 'id' => 'informanteNumDocIdentidad', ] )!!}				
+		</div>
+	</div><hr>
+
+	<div class="row" id=""  > 	
+		<div class="col-lg-3">
+			{!! Form::label ('informanteTipoDireccion','Tipo de dirección:') !!}
+				{!! Form::select ('informanteTipoDireccion',['CASA'=>'CASA','TRABAJO'=>'TRABAJO'],'informanteTipoDireccion', ['class' => 'form-control', 'id' => 'informanteTipoDireccion'] )!!}	
+		</div>
+		<div class="col-lg-5">
+			{!! Form::label ('informanteCalle','Calle:') !!}
+			{!! Form::text ('informanteCalle',old('informanteCalle'), ['class' => 'form-control mayuscula', 'id' => 'informanteCalle', ] )!!}				
+		</div>
+		<div class="col">
+			{!! Form::label ('informanteNumExterno','Número exterior:') !!}
+			{!! Form::text ('informanteNumExterno',old('informanteNumExterno'), ['class' => 'form-control mayuscula', 'id' => 'informanteNumExterno', ] )!!}				
+		</div>
+		<div class="col">
+			{!! Form::label ('informanteNumInterno','Número interior:') !!}
+			{!! Form::text ('informanteNumInterno',old('informanteNumInterno'), ['class' => 'form-control mayuscula', 'id' => 'informanteNumInterno', ] )!!}				
+		</div>
+	</div>
+
+	<div class="row" id=""  > 	
+		<div class="col-lg-4">
+			{!! Form::label ('idEstado','Estado:') !!}
+	{!! Form::select ('idEstado[]',$estados,'', ['class' => 'form-control', 'id' => 'idEstado'] )!!}				
+		</div>
+		<div class="col-lg-4">
+			{!! Form::label ('informanteidMunicipio','Municipio:') !!}
+	{!! Form::select ('informanteidMunicipio[]',$municipios,'', ['class' => 'form-control', 'id' => 'informanteidMunicipio'] )!!}				
+		</div>
+		<div class="col-lg-4">
+			{!! Form::label ('idLocalidad','Localidad:') !!}
+	{!! Form::select ('idLocalidad[]',$localidades,'', ['class' => 'form-control', 'id' => 'idLocalidad'] )!!}				
+		</div>
+	</div>
+			
+	<div class="row" id=""  > 	
+		<div class="col-lg-8">
+			{!! Form::label ('informanteidColonia','Colonia:') !!}
+			{!! Form::select ('informanteidColonia[]',$colonias,'', ['class' => 'form-control', 'id' => 'informanteidColonia'] )!!}				
+		</div>
+		<div class="col-lg-4">
+			{!! Form::label ('informanteidCodigoPostal','Código postal:') !!}
+			{!! Form::text ('informanteidCodigoPostal',old('informanteidCodigoPostal'), ['class' => 'form-control mayuscula', 'id' => 'informanteidCodigoPostal', ] )!!}				
+		</div>
+	</div><br>
+
+	<div class="row" id=""  > 
+		<div class="col-lg-2">
+			{!! Form::label ('','Tipo de telefono:') !!}
+			{!! Form::select ('',['CASA'=>'CASA','TRABAJO'=>'TRABAJO','PERSONAL'=>'PERSONAL'],'', ['class' => 'form-control', 'id' => ''] )!!}	
+		</div>
+		<div class="col-lg-2">
+			{!! Form::label ('','Lada:') !!}
+			{!! Form::select ('',['A'=>'+52','B'=>'+53','C'=>'+59'],'', ['class' => 'form-control', 'id' => 'hijos'] )!!}	
+		</div>
+		<div class="col-lg-3">
+			{!! Form::label ('informanteTelefonos','Número:') !!}
+			{!! Form::text ('informanteTelefonos',old('informanteTelefonos'), ['class' => 'form-control mayuscula', 'id' => 'informanteTelefonos', ] )!!}				
+		</div>
+		<div class="col-lg-1">
+			{!! Form::label ('','Ext:') !!}
+			{!! Form::text ('',old(''), ['class' => 'form-control mayuscula', 'id' => '', ] )!!}				
+		</div>	
+		<div class="col-lg-4">
+			{!! Form::label ('informanteCorreoElectronico','Correo electrónico:') !!}
+			{!! Form::text ('informanteCorreoElectronico',old('informanteCorreoElectronico'), ['class' => 'form-control mayuscula', 'id' => 'informanteCorreoElectronico', ] )!!}				
+		</div>
+	</div><br>
+
+	<div class="row">
+		<div class="col-lg-6">
+			<div class="form-check">
+			  <label class="form-check-label">
+			    <input class="form-check-input icheck" type="checkbox" id="informante" name="informante" value="INFORMANTE"> SÓLO INFORMANTE
+			  </label>
+			</div>
+			<div class="form-check">
+			  <label class="form-check-label">
+			    <input class="form-check-input icheck" type="checkbox"  id="recibir" value="RECIBIR"> AUTORIZADA PARA DAR, OIR Y RECIBIR INFORMES
+			  </label>
+			</div>
+		</div>
+	</div>
+	</div>
+
+
+
+</div>
+<div class="my-4">	</div>
+
 			      </div>
 			      <div class="modal-footer">
-			        <button type="button" class="btn btn-primary">GUARADR CAMBIOS</button>
-			        <button type="button" class="btn btn-secondary" data-dismiss="modal">CERRAR</button>
+			        <button type="button" class="btn btn-dark">GUARDAR CAMBIOS</button>
+			        <button type="button" class="btn btn-danger" data-dismiss="modal">CERRAR</button>
 			      </div>
 			    </div>
 			  </div>
