d6c2c59b625c3c6a3d0a9d5aab06bebd4f05c0bd
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-04-05 21:55:43 -0500

    Validando campos del blade persona entrevistador

--- resources/views/desaparecidos/secciones/seccion1_persona_entrevistador.blade.php
+++ resources/views/desaparecidos/secciones/seccion1_persona_entrevistador.blade.php
@@ -38,7 +38,7 @@
 				</div>
 				<div class="col" id="otro_dialec" style="display:none">
 					{!! Form::label ('','Especifique:') !!}
-					{!! Form::text ('',old('Nombre del intérprete'), ['class' => 'form-control mayuscula', 'placeholder' => 'Escribe el nombre del idioma o dialecto', 'id' => '', 'data-validation' => 'required', 'data-validation-error-msg-required' => 'Ingrese otro dialecto','data-validation-depends-on' => 'entrevistadorIdioma','data-validation-depends-on-value' =>'OTRO'] )!!}
+					{!! Form::text ('otroIdioma',old('Nombre del intérprete'), ['class' => 'form-control mayuscula', 'placeholder' => 'Escribe el nombre del idioma o dialecto', 'id' => 'otroIdioma', 'data-validation' => 'required', 'data-validation-error-msg-required' => 'Ingrese otro dialecto','data-validation-depends-on' => 'entrevistadorIdioma','data-validation-depends-on-value' =>'OTRO'] )!!}
 			  	</div>
 				
 		</div>
@@ -46,12 +46,12 @@
   		<div class="row">
   			<div class="col-lg-4" id="otro_dialec2" style="display:none">
 					{!! Form::label ('','Nombre del intérprete:') !!}
-					{!! Form::text ('',old('Nombre del intérprete'), ['class' => 'form-control mayuscula', 'placeholder' => 'Escribe el nombre', 'id' => 'interpreteNombre', 'data-validation' => 'required','data-validation-depends-on' => 'entrevistadorIdioma','data-validation-depends-on-value' =>'OTRO'] )!!}
+					{!! Form::text ('',old('Nombre del intérprete'), ['class' => 'form-control mayuscula', 'placeholder' => 'Escribe el nombre', 'id' => 'interpreteNombre', 'data-validation' => 'required','data-validation-depends-on' => 'entrevistadorIdioma','data-validation-depends-on-value' =>'OTRO','data-validation-error-msg-required' => 'Ingrese nombre del intérprete'] )!!}
 			  	</div>
 
 			  	<div class="col" id="otro_dialec3" style="display:none">
 					{!! Form::label ('','Organización o institución:') !!}
-					{!! Form::text ('',old('Nombre de la organización o institución'), ['class' => 'form-control mayuscula','placeholder' => 'Escribe el nombre de la organización o institución','id' => 'interpreteOrganizacion', 'data-validation' => 'required','data-validation-depends-on' => 'entrevistadorIdioma','data-validation-depends-on-value' =>'OTRO'] )!!}
+					{!! Form::text ('',old('Nombre de la organización o institución'), ['class' => 'form-control mayuscula','placeholder' => 'Escribe el nombre de la organización o institución','id' => 'interpreteOrganizacion', 'data-validation' => 'required','data-validation-depends-on' => 'entrevistadorIdioma','data-validation-depends-on-value' =>'OTRO', 'data-validation-error-msg-required' => 'Ingrese nombre de la organización'] )!!}
 			  	</div>
 		</div><hr>
 		<div class="row">
@@ -62,7 +62,7 @@
 			</div>
 			<div class="col-lg-4" id="cuando" style="display:none">
 				{!! Form::label ('','¿Cuándo?') !!}
-				{!! Form::text ('fecha_visita',old('fecha_visita'), ['class' => 'form-control mayuscula', 'id' => 'fecha_visita','placeholder' => 'DD/MM/AAAA','data-validation-depends-on' => 'entrevistadorPrimeraVez','data-validation-depends-on-value' =>'NO','data-validation-format'=>"dd-mm-yyyy" ] )!!}
+				{!! Form::text ('fecha_visita',old('fecha_visita'), ['class' => 'form-control mayuscula', 'id' => 'fecha_visita','placeholder' => 'DD/MM/AAAA','data-validation-depends-on' => 'entrevistadorPrimeraVez','data-validation-depends-on-value' =>'NO', 'data-validation' =>'date','data-validation-format'=>"dd-mm-yyyy" , 'data-validation-error-msg-date' => 'Ingrese fecha correcta'] )!!}
 				
 			</div>
 		</div>
@@ -70,6 +70,32 @@
 </div>
 <div class="my-4">	</div>
 
-<b>Nota:</b>En la linea 36 cambiar id por "entrevistadorIdioma" en el metodo js ;
-<br>
-<b>Nota:</b>En la linea 60 cambiar id por "entrevistadorPrimeraVez" en el metodo js ;
+<!-- Mover Script datosgral.js -->
+<script type="text/javascript">
+	$(document).ready(function(){
+	    $("#entrevistadorNombre").keypress(function(event){
+	        var inputValue = event.which;
+	        // allow letters and whitespaces only.
+	        if(!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0)) { 
+	            event.preventDefault(); 
+	        }
+	    });
+
+	    $("#entrevistadorNombre").prop('disabled', true);
+	    $("#entrevistadorCargo").prop('disabled', true);
+	    $("#interpreteNombre").keypress(function(event){
+	        var inputValue = event.which;
+	        // allow letters and whitespaces only.
+	        if(!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0)) { 
+	            event.preventDefault(); 
+	        }
+	    });
+	    $("#otroIdioma").keypress(function(event){
+	        var inputValue = event.which;
+	        // allow letters and whitespaces only.
+	        if(!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0)) { 
+	            event.preventDefault(); 
+	        }
+	    });
+	});
+</script>
