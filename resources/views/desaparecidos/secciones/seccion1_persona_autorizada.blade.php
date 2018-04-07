95101b75a7e7bf43ec9c43ac0c887a2df27f0e96
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-04-06 19:34:26 -0500

    implementando mockup en la seccion de persona autorizada

--- resources/views/desaparecidos/secciones/seccion1_persona_autorizada.blade.php
+++ resources/views/desaparecidos/secciones/seccion1_persona_autorizada.blade.php
@@ -3,7 +3,7 @@
 		<h5 class="card-title">PERSONAS AUTORIZADAS PARA DAR, OIR Y RECIBIR INFORMES</h5>		
 	</div>
 	<div class="card-body">
-		<table class="table table-hover">
+		<table class="table table-hover" id="tablePrueba">
 		  <thead>
 		    <tr>
 		      <th scope="col">No.</th>
@@ -14,14 +14,14 @@
 		      <th scope="col">Acciones</th>
 		    </tr>
 		  </thead>
-		  <tbody>
+		  <tbody id='cuerpoT'>
 		    <tr>
 		      <th scope="row">1</th>
-		      <td>SELENE PATRICIA GARCIA RAMON</td>
-		      <td>MADRE</td>
-		      <td>2281459373</td>
+		      <td>{{ Session::get('nombre')}} {{ Session::get('apellido1')}} {{Session::get('apellido2')}}</td>
+		      <td>{{Session::get('parentesco')}}</td>
+		      <td>{{Session::get('telefono')}}</td>
 		      <td>INFORMANTE / RECIBIR INFORMACIÓN</td>
-		      <td><button type="button" class="btn btn-dark" data-toggle="modal" data-target="#mostrarModal" id="prueba">MOSTRAR</button></td>
+		      <td><button type="button" class="btn btn-dark"  id="prueba">MOSTRAR</button></td>
 		    </tr>
 		  </tbody>
 		</table>
@@ -50,3 +50,39 @@
 	</div>
 </div>
 <div class="my-4">	</div>
+
+<script type="text/javascript">
+	$('#prueba').click(function(){
+        console.log('Entrando a los hijos');
+        tr = $('<tr>');
+        td1 =$('<td>');
+        td2 =$('<td>');
+        td3 =$('<td>');
+        td4 =$('<td>');
+        td5 =$('<td>');
+        td6 =$('<td>');
+
+        td1.text('*');
+        td2.text("{{ Session::get('nombre')}} {{ Session::get('apellido1')}} {{Session::get('apellido2')}}");
+        td3.text("{{Session::get('parentesco')}}");
+        td4.text("{{Session::get('telefono')}}");
+        td5.text('INFORMANTE / RECIBIR INFORMACIÓN');
+        td6.append("<button type='button' class='btn btn-dark  id='prueba'>MOSTRAR</button>");
+        
+
+
+
+        tr.append(td1);
+        tr.append(td2);
+        tr.append(td3);
+        tr.append(td4);
+        tr.append(td5);
+        tr.append(td6);
+
+        $('#cuerpoT').append(tr);
+
+
+   
+        
+    })
+</script>
