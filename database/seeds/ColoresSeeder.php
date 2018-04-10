3f470bed42c9582452a13a55f10ae4bb89aa3bd0
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-04-10 18:15:51 -0500

    modificando seeder para colores, marcas y tipos de calzado

--- database/seeds/ColoresSeeder.php
+++ database/seeds/ColoresSeeder.php
@@ -11,21 +11,22 @@ class ColoresSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('cat_colores')->insert([
-        	['nombre' => 'AZÚL',					'image' => '1.jpg'],        
-        	['nombre' => 'ROJO',					'image' => '2.jpg'],
-        	['nombre' => 'MORADO',					'image' => '3.jpg'],
-        	['nombre' => 'VERDE',					'image' => '4.jpg'],
-        	['nombre' => 'NEGRO',					'image' => '5.jpg'],
-            ['nombre' => 'BLANCO',        			'image' => '6.jpg'],
-            ['nombre' => 'ROSA',               		'image' => '7.jpg'],
-        	['nombre' => 'NARANJA',					'image' => '8.jpg'],
-        	['nombre' => 'AMARILLO',				'image' => '9.jpg'], 
-        	['nombre' => 'GRIS',        			'image' => '10.jpg'],
-            ['nombre' => 'PLATEADO',               	'image' => '11.jpg'],
-        	['nombre' => 'DORADO',					'image' => '12.jpg'],
-        	['nombre' => 'CAFÉ',					'image' => '13.jpg'],
-        	['nombre' => 'OTRO',					'image' => '14.jpg'],    	
+        //
+        DB::table('cat_calzado_color')->insert([
+            ['nombre' => 'AZÚL',                    'image' => '1.jpg'],        
+            ['nombre' => 'ROJO',                    'image' => '2.jpg'],
+            ['nombre' => 'MORADO',                  'image' => '3.jpg'],
+            ['nombre' => 'VERDE',                   'image' => '4.jpg'],
+            ['nombre' => 'NEGRO',                   'image' => '5.jpg'],
+            ['nombre' => 'BLANCO',                  'image' => '6.jpg'],
+            ['nombre' => 'ROSA',                    'image' => '7.jpg'],
+            ['nombre' => 'NARANJA',                 'image' => '8.jpg'],
+            ['nombre' => 'AMARILLO',                'image' => '9.jpg'], 
+            ['nombre' => 'GRIS',                    'image' => '10.jpg'],
+            ['nombre' => 'PLATEADO',                'image' => '11.jpg'],
+            ['nombre' => 'DORADO',                  'image' => '12.jpg'],
+            ['nombre' => 'CAFÉ',                    'image' => '13.jpg'],
+            ['nombre' => 'OTRO',                    'image' => '14.jpg'],       
         ]);
     }
 }
