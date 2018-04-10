81497aa119eb1c68f3d2fef580750d3a3d471857
ADFGE\efernandezl <efernandezl@fiscaliaveracruz.gob.mx>
2018-04-10 17:14:12 -0500

    Reestructuracion de las etiquetas en plantilla datosgral

--- resources/views/desaparecidos/datosgral.blade.php
+++ resources/views/desaparecidos/datosgral.blade.php
@@ -12,49 +12,116 @@
 		<div class="row">
 			<div class="col">
 				{!! Form::label ('nombres','Nombres(s):') !!}
-				{!! Form::text ('nombres',old('Nombres(s)'), ['class' => 'form-control mayuscula', 'id' => 'nombres', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Ingresa un nombre'] )!!}
+				{!! Form::text ('nombres',
+								old('Nombres(s)'), 
+								['class' => 'form-control mayuscula',
+									'placeholder' => 'Escribe el/los nombre(s)', 
+									'id' => 'nombres', 
+									'data-validation' => 'required', 
+									'data-validation-error-msg-required' => '* Ingresa un nombre'
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('primerAp','Primer apellido:') !!}
-				{!! Form::text ('primerAp',old('Apellido materno'), ['class' => 'form-control mayuscula', 'id' => 'primerAp', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Ingresa un apellido'] )!!}
+				{!! Form::text ('primerAp',
+								old('Apellido materno'), 
+								['class' => 'form-control mayuscula',
+									'placeholder' => 'Escribe el primer apellido', 
+									'id' => 'primerAp', 
+									'data-validation' => 'required', 	
+									'data-validation-error-msg-required' => '* Ingresa un apellido'
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('segundoAp','Segundo apellido:') !!}
-				{!! Form::text ('segundoAp',old('Apellido materno'), ['class' => 'form-control mayuscula', 'id' => 'segundoAp'] )!!}
+				{!! Form::text ('segundoAp',
+								old('Apellido materno'), 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe el segundo apellido',
+									'id' => 'segundoAp'
+								] )!!}
 			</div>
 		</div>
 		<div class="row">
 			<div class="col">
 				{!! Form::label ('apodo','Apodo (alias):') !!}
-				{!! Form::text ('apodo',old('apodo'), ['class' => 'form-control mayuscula', 'id' => 'apodo' ] )!!}
+				{!! Form::text ('apodo',
+								old('apodo'), 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe el apodo',
+									'id' => 'apodo' 
+								] )!!}
 			</div>			
 		</div>
 		<div class="row">
 			<div class="col">
 				{!! Form::label ('edadExtravio','Edad de extravío:') !!}
-				{!! Form::text ('edadExtravio',old('EdadExtravio'), ['class' => 'form-control', 'id' => 'edadExtravio', 'data-validation' => 'required number length' ,  'data-validation-error-msg' => '* Ingresar edad',  'data-validation-length'=>"0-3",'data-mask'=>"000"] )!!}
+				{!! Form::text ('edadExtravio',
+								old('EdadExtravio'), 
+								['class' => 'form-control',
+									'placeholder' => 'Escribe la edad', 	
+									'id' => 'edadExtravio', 
+									'data-validation' => 'required number length' ,  
+									'data-validation-error-msg' => '* Ingresar edad',  'data-validation-length'=>"0-3",'data-mask'=>"000"
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('edadAparente','Edad aparente:') !!}
-				{!! Form::text ('edadAparente',old('Edad aparente'), ['class' => 'form-control', 'id' => 'edadAparente', 'data-validation' => 'required number length', 'data-validation-error-msg' => '* Ingrese una edad aparente',  'data-validation-length'=>"0-3",'data-mask'=>"000"] )!!}
+				{!! Form::text ('edadAparente',
+								old('Edad aparente'), 
+								['class' => 'form-control', 
+									'placeholder' => 'Escribe la edad aparente',
+									'id' => 'edadAparente', 
+									'data-validation' => 'required number length', 
+									'data-validation-error-msg' => '* Ingrese una edad aparente',  'data-validation-length'=>"0-3",'data-mask'=>"000"
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('fechaNacimiento','Fecha de nacimiento:') !!}
-				{!! Form::text ('fechaNacimiento',old('Fecha de nacimiento'), ['class' => 'form-control', 'id' => 'fechaNacimiento' , 'data-validation' => 'required date', 'data-validation-error-msg' => '* Ingrese o corrija la fecha de nacimiento','data-validation-format'=>"dd/mm/yyyy"] )!!}
+				{!! Form::text ('fechaNacimiento',
+								old('Fecha de nacimiento'), 
+								['class' => 'form-control', 
+									'placeholder' => 'Escribe la fecha',
+									'id' => 'fechaNacimiento' , 
+									'data-validation' => 'required date', 
+									'data-validation-error-msg' => '* Ingrese o corrija la fecha de nacimiento',
+									'data-validation-format'=>"dd/mm/yyyy"
+								] )!!}
 			</div>
 		</div>
 		<div class="row">
 			<div class="col">
 				{!! Form::label ('idNacionalidad','Nacionalidad:') !!}
-				{!! Form::select ('idNacionalidad',$nacionalidades,'', ['class' => 'form-control', 'id' => 'idNacionalidad', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Elija una opción'] )!!}
+				{!! Form::select ('idNacionalidad',
+									$nacionalidades,
+									'', 
+									['class' => 'form-control', 
+										'id' => 'idNacionalidad', 
+										'data-validation' => 'required', 
+										'data-validation-error-msg-required' => '* Elija una opción'
+									] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('idEscolaridad','Escolaridad:') !!}
-				{!! Form::select ('idEscolaridad',$escolaridades,'', ['class' => 'form-control', 'id' => 'escolaridad' , 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Elija una opción'] )!!}
+				{!! Form::select ('idEscolaridad',
+									$escolaridades,	
+									'', 
+									['class' => 'form-control', 
+										'id' => 'escolaridad' , 
+										'data-validation' => 'required', 
+										'data-validation-error-msg-required' => '* Elija una opción'
+									] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('idOcupacion','Ocupación:') !!}
-				{!! Form::select ('idOcupacion',$ocupaciones,'', ['class' => 'form-control', 'id' => 'ocupacion', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Elija una opción'] )!!}
+				{!! Form::select ('idOcupacion',
+									$ocupaciones,
+									'', 
+									['class' => 'form-control', 
+										'id' => 'ocupacion', 
+										'data-validation' => 'required', 
+										'data-validation-error-msg-required' => '* Elija una opción'
+									] )!!}
 			</div>			
 		</div>
 		<div class="row">
@@ -63,15 +130,34 @@
 			</div>
 			<div class="col">
 				{!! Form::label ('nombres','Nombre de la madre:') !!}
-				{!! Form::text ('familiaresNombres[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresNombres', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Ingrese un nombre'] )!!}
+				{!! Form::text ('familiaresNombres[]',
+								'', 	
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe el nombre',
+									'id' => 'familiaresNombres', 
+									'data-validation' => 'required', 
+									'data-validation-error-msg-required' => '* Ingrese un nombre'
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('primerAp','Primer apellido:') !!}
-				{!! Form::text ('familiaresPrimerAp[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresPrimerAp', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Ingrese el primer apellido'] )!!}
+				{!! Form::text ('familiaresPrimerAp[]',
+								'', 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe el primer apellido',
+									'id' => 'familiaresPrimerAp', 
+									'data-validation' => 'required', 
+									'data-validation-error-msg-required' => '* Ingrese el primer apellido'
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('segundoAp','Segundo apellido:') !!}
-				{!! Form::text ('familiaresSegundoAp[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresSegundoAp'] )!!}
+				{!! Form::text ('familiaresSegundoAp[]',
+								'', 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe el segundo apellido',
+									'id' => 'familiaresSegundoAp'
+								] )!!}
 			</div>
 			<div>
 				{{ Form::hidden('familiaresEdad[]', '0') }}
@@ -83,15 +169,34 @@
 			</div>
 			<div class="col">
 				{!! Form::label ('nombres','Nombre del padre:') !!}
-				{!! Form::text ('familiaresNombres[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresNombres', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Ingrese un nombre'] )!!}
+				{!! Form::text ('familiaresNombres[]',
+								'', 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe el nombre',
+									'id' => 'familiaresNombres', 
+									'data-validation' => 'required', 
+									'data-validation-error-msg-required' => '* Ingrese un nombre'
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('primerAp','Primer apellido:') !!}
-				{!! Form::text ('familiaresPrimerAp[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresPrimerAp', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Ingrese el primer apellido'] )!!}
+				{!! Form::text ('familiaresPrimerAp[]',
+								'', 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe el segundo apellido',
+									'id' => 'familiaresPrimerAp', 
+									'data-validation' => 'required', 
+									'data-validation-error-msg-required' => '* Ingrese el primer apellido'
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('segundoAp','Segundo apellido') !!}
-				{!! Form::text ('familiaresSegundoAp[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresSegundoAp'] )!!}
+				{!! Form::text ('familiaresSegundoAp[]',
+								'', 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe el segundo apellido',
+									'id' => 'familiaresSegundoAp'
+								] )!!}
 			</div>
 			<div>
 				{{ Form::hidden('familiaresEdad[]', '0') }}
@@ -100,7 +205,12 @@
 		<div class="row">
 			<div class="col">
 				{!! Form::label ('idEdocivil','Estado civil:') !!}
-	        	{!! Form::select ('idEdocivil',$edoscivil ,'', ['class' => 'form-control', 'id' => 'idEdocivil'] )!!}
+	        	{!! Form::select ('idEdocivil',
+	        						$edoscivil ,
+	        						'', 
+	        						['class' => 'form-control', 
+	        							'id' => 'idEdocivil'
+	        						] )!!}
 			</div>			
 		</div>
 		<div class="row" id="nombrePareja" style="display:none">
@@ -109,16 +219,34 @@
 			</div>			
 			<div class="col">
 				{!! Form::label ('nombres','Nombre de la pareja:') !!}
-				{!! Form::text ('familiaresNombres[]','', ['class' => 'form-control mayuscula', 'id' =>'familiaresNombres
-			', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Ingrese un nombre'] )!!}
+				{!! Form::text ('familiaresNombres[]',
+									'', 
+									['class' => 'form-control mayuscula',
+										'placeholder' => 'Escribe el nombre', 
+										'id' =>'familiaresNombres', 
+										'data-validation' => 'required', 
+										'data-validation-error-msg-required' => '* Ingrese un nombre'
+									] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('primerAp','Primer apellido:') !!}
-				{!! Form::text ('familiaresPrimerAp[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresPrimerAp', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Ingrese el primero apellido'] )!!}
+				{!! Form::text ('familiaresPrimerAp[]',
+									'', 
+									['class' => 'form-control mayuscula', 
+										'placeholder' => 'Escribe el primer apellido',
+										'id' => 'familiaresPrimerAp', 
+										'data-validation' => 'required', 
+										'data-validation-error-msg-required' => '* Ingrese el primero apellido'
+									] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('segundoAp','Segundo apellido:') !!}
-				{!! Form::text ('familiaresSegundoAp[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresSegundoAp'] )!!}
+				{!! Form::text ('familiaresSegundoAp[]',
+								'', 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe el segundo apellido',
+									'id' => 'familiaresSegundoAp'
+								] )!!}
 			</div>
 			<div>
 				{{ Form::hidden('familiaresEdad[]', '0') }}
@@ -138,7 +266,12 @@
 	<div class="card-header">
 		<div class="col-4">
 			{!! Form::label ('hijos','Hijos:') !!}
-			{!! Form::select ('hijos',['NO'=>'NO','SI'=>'SI'],'', ['class' => 'form-control', 'id' => 'hijos' ] )!!}			
+			{!! Form::select ('hijos',
+								['NO'=>'NO','SI'=>'SI'],
+								'', 
+								['class' => 'form-control', 
+									'id' => 'hijos' 
+								] )!!}			
 		</div>
 		<button type="button" style="display:none" id="btnAddHijo" class="btn btn-primary float-right">Agregar hijo</button>
 		
@@ -150,22 +283,44 @@
 			</div>
 			<div class="col">
 				{!! Form::label ('nombres','Nombres(s):') !!}
-				{!! Form::text ('familiaresNombres[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresNombres', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Ingrese un nombre'] )!!}
+				{!! Form::text ('familiaresNombres[]',
+								'', 
+								['class' => 'form-control mayuscula',
+									'placeholder' => 'Escribe el nombre', 
+									'id' => 'familiaresNombres', 
+									'data-validation' => 'required', 
+									'data-validation-error-msg-required' => '* Ingrese un nombre'
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('primerAp','Primer apellido:') !!}
-				{!! Form::text ('familiaresPrimerAp[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresPrimerAp', 'data-validation' => 'required', 'data-validation-error-msg-required' => '* Ingrese el primer apellido'] )!!}
+				{!! Form::text ('familiaresPrimerAp[]',
+									'', ['class' => 'form-control mayuscula', 
+											'placeholder' => 'Escribe el primer apellido',
+											'id' => 'familiaresPrimerAp', 
+											'data-validation' => 'required', 
+											'data-validation-error-msg-required' => '* Ingrese el primer apellido'
+										] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('segundoAp','Segundo apellido:') !!}
-				{!! Form::text ('familiaresSegundoAp[]','', ['class' => 'form-control mayuscula', 'id' => 'familiaresSegundoAp'] )!!}
+				{!! Form::text ('familiaresSegundoAp[]',
+								'', 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe el segundo apellido',
+									'id' => 'familiaresSegundoAp'
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('familiaresEdad', 'Edad:') !!}
-				{!! Form::text ('familiaresEdad[]','', ['class' => 'form-control',
-																 'id' => 'familiaresFamiliaresEdad',										 
-																 'data-validation' => 'number',
-																 'data-validation-permitted' => 'range [1; 100]', 'data-validation-error-msg-required' => '* Solo puedes ingresar números'] )!!}
+				{!! Form::text ('familiaresEdad[]',
+								'', 
+								['class' => 'form-control',
+									'placeholder' => 'Escribe la edad',
+									'id' => 'familiaresFamiliaresEdad',										 
+									 'data-validation' => 'number',
+									 'data-validation-permitted' => 'range [1; 100]', 'data-validation-error-msg-required' => '* Solo puedes ingresar números'
+								 ] )!!}
 			</div>			
 		</div>
 	</div>
@@ -177,28 +332,53 @@
 	<div class="card-header">
 		<div class="col">
 			{!! Form::label ('embarazo','Esta embarazada:') !!}
-			{!! Form::select ('embarazo',['NO'=>'NO','SI'=>'SI', 'LO IGNORAN' => 'LO IGNORAN'],'', ['class' => 'form-control', 'id' => 'embarazo'] )!!}
+			{!! Form::select ('embarazo',
+								['NO'=>'NO','SI'=>'SI', 'LO IGNORAN' => 'LO IGNORAN'],
+								'', 
+								['class' => 'form-control', 
+									'id' => 'embarazo'
+								] )!!}
 		</div>		
 	</div>
 	<div class="card-body">
 		<div class="row"  id="datosEmbarazo" style="display:none">
 			<div class="col">
 				{!! Form::label ('gestacionSemanas','Semanas:') !!}
-				{!! Form::text ('gestacionSemanas',old('gestacionSemanas'), ['class' => 'form-control mayuscula', 'id' => 'gestacionSemanas'] )!!}
+				{!! Form::text ('gestacionSemanas',
+								old('gestacionSemanas'), 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe las semanas',
+									'id' => 'gestacionSemanas'
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('gestacionMeses','Meses:') !!}
-				{!! Form::text ('gestacionMeses',old('gestacionMeses'), ['class' => 'form-control mayuscula', 'id' => 'gestacionMeses'] )!!}
+				{!! Form::text ('gestacionMeses',
+								old('gestacionMeses'), 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe los meses',
+									'id' => 'gestacionMeses'
+								] )!!}
 			</div>
 			<div class="col">
 				{!! Form::label ('rumoresBebe','Rumores sobre el nacimiento:') !!}
-				{!! Form::select ('rumoresBebe',['NO'=>'NO','SI'=>'SI', 'LO IGNORAN' => 'LO IGNORAN'],'', ['class' => 'form-control', 'id' => 'rumoresBebe'] )!!}
+				{!! Form::select ('rumoresBebe',
+									['NO'=>'NO','SI'=>'SI', 'LO IGNORAN' => 'LO IGNORAN'],
+									'', 
+									['class' => 'form-control', 
+										'id' => 'rumoresBebe'
+									] )!!}
 			</div>	
 		</div>
 		<div class="row" id="datosEmbarazo3" style="display:none">
 			<div class="col-12">
 				{!! Form::label ('pormenores','Pormenores:') !!}
-				{!! Form::text ('pormenores',old('Pormenores'), ['class' => 'form-control mayuscula', 'id' => 'pormenores'] )!!}
+				{!! Form::text ('pormenores',
+								old('Pormenores'), 
+								['class' => 'form-control mayuscula', 
+									'placeholder' => 'Escribe los pormenores',
+									'id' => 'pormenores'
+								] )!!}
 			</div>
 		</div>
 	</div>
