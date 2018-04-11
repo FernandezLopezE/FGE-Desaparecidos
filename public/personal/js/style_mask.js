27f8ab9b53a422ae7ae9bb111c90f8354c872bc7
Carlos Avila Aguilar <cavila@fiscaliaveracruz.gob.mx>
2018-04-10 19:39:58 -0500

    Creando un archivo js para el manejo de mascaras de la del frontend

---
+++ public/personal/js/style_mask.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	/************************************************
+			MASCARAS DEL ENTREVISTADOR
+	*************************************************/
+	$('input[name="fechaVisita"]').mask('00/00/0000');
+
+	$('input[name="fechaNacimiento"]').mask('00-00-0000');
+	
+})
