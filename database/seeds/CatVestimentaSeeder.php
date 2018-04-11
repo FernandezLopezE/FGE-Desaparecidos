7d735b122e82f21a02a17a43eb583ad36930829b
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-04-10 22:42:51 -0500

    Cambio de lugar de los campos predeterminados

--- database/seeds/CatVestimentaSeeder.php
+++ database/seeds/CatVestimentaSeeder.php
@@ -12,14 +12,14 @@ class CatVestimentaSeeder extends Seeder
     public function run()
     {
         DB::table('cat_vestimenta')->insert([
-            [ 'id' => 1, 'nombre' => 'CIVIL'],
-            [ 'id' => 2, 'nombre' => 'FORMAL'],
-            [ 'id' => 3, 'nombre' => 'INFORMAL'],
-            [ 'id' => 4, 'nombre' => 'DEPORTIVO'],
-            [ 'id' => 5, 'nombre' => 'BEBÉ'],
-            [ 'id' => 6, 'nombre' => 'MARINA'],
-            [ 'id' => 7, 'nombre' => 'ESCOLAR'],
-            [ 'id' => 8, 'nombre' => 'SIN INFORMACIÓN']
+            [ 'id' => 1, 'nombre' => 'SIN INFORMACIÓN'],
+            [ 'id' => 2, 'nombre' => 'CIVIL'],
+            [ 'id' => 3, 'nombre' => 'FORMAL'],
+            [ 'id' => 4, 'nombre' => 'INFORMAL'],
+            [ 'id' => 5, 'nombre' => 'DEPORTIVO'],
+            [ 'id' => 6, 'nombre' => 'BEBÉ'],
+            [ 'id' => 7, 'nombre' => 'MARINA'],
+            [ 'id' => 8, 'nombre' => 'ESCOLAR']
         ]);//
     }
 }
