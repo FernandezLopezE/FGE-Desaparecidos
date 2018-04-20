fb567eb60bdd8b103cb4482cc2408d5957463381
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-04-19 19:41:24 -0500

    Creando Modelos para antecedentes medicos

---
+++ app/Models/CatAdicciones.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CatAdicciones extends Model
+{
+    //
+    protected $table = 'cat_adicciones';
+	protected $fillable = [
+		'nombre',	
+	];
+}
