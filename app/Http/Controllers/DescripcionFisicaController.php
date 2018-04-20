33d345635f4adea06a4d7f17e299743d606c3c91
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-04-19 22:39:44 -0500

    Controlador para descripcion fisica de la persona

---
+++ app/Http/Controllers/DescripcionFisicaController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DescripcionFisicaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($idDesaparecido)
+    {
+        //
+        $desaparecido = Cedula::find($idDesaparecido);
+
+        $complexiones = \App\Models\CatComplexion::all()->pluck('nombre','id');
+        $coloresPiel = \App\Models\CatColorPiel::all()->pluck('nombre','id');
+        $subParticularidades = \App\Models\CatSubParticularidades::all()->pluck('nombre','id');
+        $subModificaciones = \App\Models\CatSubModificaciones::all()->pluck('nombre','id');
+
+
+        return view('descripcionfisica.form_descripcion_fisica',
+            [
+                'desaparecido' => $desaparecido,
+                'complexiones' => $complexiones,
+                'coloresPiel' => $coloresPiel,
+                'particularidades' => $subParticularidades,
+                'modificaciones' => $subModificaciones,
+            ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
