1001ae91352a6d7071e5682699302751e1dbcf41
ADFGE\dlanda <proyectosmx94@gmail.com>
2018-04-30 22:02:38 -0500

    Creación de modal de cabello y cabeza

---
+++ resources/views/includes/partesCuerpo/01ModalCabello.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="modalCabello" rle="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Titulo aqui</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <!-- Contenido del formulario-->
+        
+        <!-- Fin del Contenido del formulario-->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">CERRAR</button>
+      </div>
+    </div>
+  </div>
+</div>
