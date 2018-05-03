637ed6db5e416fc56cbe90d008dc4b42d4796186
ADFGE\AlejandroFernandezToledo <afernandezt@fiscaliaveracruz.gob.mx>
2018-05-03 16:05:26 -0500

    definiendo la base exacta con actualizacion de tipo de letra y tamaño

--- resources/views/plantillas/24-albergues.blade.php
+++ resources/views/plantillas/24-albergues.blade.php
@@ -52,21 +52,14 @@
 			
 		}
 
-		div#foto{
-			float:right;
-			margin-left:20px;
-			margin-right: 35px;
-			width: 250px;
-			height:300px;
-			
-		}
 		
 
 		
 		
 		
-		p{ font-size:20px;
-		font-family: "calibri", Garamond, 'Comic Sans'; }
+		p.fuente { font-size:18px;
+				font-family: Arial, Helvetica, sans-serif; 
+		}
 
 		#sangria p {text-indent:50px ; margin:0px ; padding:0px ; border:0px}
 	
@@ -84,54 +77,55 @@
 		
 <div id="cuerpo">
 	
-	<p><strong> LIC. </strong>	<strong> {{--{!! $datos->nombreRemitente !!}--}} </strong>
+	<p class="fuente"><strong> LIC. </strong>	<strong> {{--{!! $datos->nombreRemitente !!}--}} </strong>
 	</p>
 	
 	
-	 <p><strong> Xalapa, Ver. </strong>	<strong> {{--{!! $datos->fechaHoy !!}--}}  </strong></p>
+	 <p class="fuente"><strong> Xalapa, Ver. </strong>	<strong> {{--{!! $datos->fechaHoy !!}--}}  </strong></p>
 	
 	
 	
-	<p> <strong> No. de Inv. </strong><strong> {{--{!! $datos->numCarpeta !!} --}} </strong></p>
+	<p class="fuente"> <strong> No. de Inv. </strong><strong> {{--{!! $datos->numCarpeta !!} --}} </strong></p>
 	
 	
 	
-	<p><strong> OFICIO NO. FGE/FIM/FEADPD/</strong>	<strong> {{--{!! $datos->numOficio !!}/{!! $datos->anio !!} --}} </strong></p>
+	<p class="fuente"><strong> OFICIO NO. FGE/FIM/FEADPD/</strong>	<strong> {{--{!! $datos->numOficio !!}/{!! $datos->anio !!} --}} </strong></p>
 	
-	<br><br>
-	<p align="left"><strong> PRESENTE</strong></p>	
+	<br>
+	<p class="fuente" align="left"><strong> PRESENTE</strong></p>	
 	
 	<div id="sangria">
 
-		<p align="justify">
+		<p align="justify"  class="fuente">
 			Con fundamento en lo que disponen el artículo 19, 20 y 21 de la Constitución General de la República; 67 Fracción I apartado A de la Constitución Política Local; <strong>{{--{!! $datos->articulos !!} --}} </strong></p>
 		<br>	
-		<p align="justify">
+		<p align="justify" class="fuente">
 			El Protocolo Homologado para la Búsqueda de Personas Desaparecidas y la Investigación del Delito de Desaparición Forzada; los acuerdos 025/2011, 01/2013, 023/2013, emitidos por el Procurador General de Justicia del Estado; acuerdo General número 07/2017 signado por el Fiscal General del Estado de Veracruz; I, II, V, VI y XVII, de la Declaración Americana de los Derechos y Deberes del Hombre; 1, 2, 3, 7 y 8 de la Declaración Universal de Derechos Humanos; 1, 3, 6 numero 1; 9 numero 1; 27 y 26 del Pacto Internacional de Derechos Civiles y Políticos; 3 y 5 del Pacto Internacional de Derechos Económicos, Sociales y Culturales; 8, 27 núm. 1, 9, 24 y 25 de la Convención Americana sobre Derechos Humanos, “Pacto San José”; 1, 8, 12 incisos a) y b) y 27) de los Principios y directrices básicas sobre el derecho de las víctimas de violaciones manifiestas de las normas Internacionales de Derechos Humanos y de violaciones graves del derecho internacional humanitario a interponer recursos y obtener reparaciones; 1, 2 y 3 de la Declaración Sobre los Principios Fundamentales  de justicia para las víctimas del delito y del abuso de poder; así como el artículo 7 Fracción X, de la Ley General de Victimas, le solicito su colaboración para con esta Representación Social, en la búsqueda y localización y presentación del ciudadano <strong>{{--{!! $datos->desaparecidoNombre !!} --}}</strong>, quien se encuentra desaparecido, siendo visto por última vez en <strong>{{--{!! $datos->desaparecidoLugar !!} --}}</strong>, el día <strong>{{--{!! $datos->desaparecidoFecha !!} --}}</strong>, a las <strong>{{--{!! $datos->desaparecidoHora !!} --}}</strong>  horas, así como del vehículo en el que viajaba siendo un: <strong>{{--{!! $datos->vehiculoDescripcion !!} --}}</strong> para lo cual anexo al presente:
+		
 		</p>
 	</div>
 	<div>
-		<p> Cedula de Boletín de Persona Extraviada; y</p>		
-		<p>	Registro Único de Persona Desaparecida;</p>
+		<p class="fuente"> Cedula de Boletín de Persona Extraviada; y</p>		
+		<p class="fuente">	Registro Único de Persona Desaparecida;</p>
 	</div>
 
 	<div id="sangria">
 
-		<p>
+		<p class="fuente">
 			Lo anterior a efecto de que las documentales antes señaladas sean distribuidas en los distintos DIF municipales, casas de Protección Social, Albergues, orfanatos según sea el caso, de este Estado y sean colocadas en lugares visibles al público en general, ya que estas acciones contribuyen a la búsqueda eficiente del ciudadano  <strong>{{--{!! $datos->desaparecidoNombre !!} --}}</strong>.
 		</p>
 
-		<p>
+		<p class="fuente">
 			Así también deberá informar lo siguiente:
 		</p>
 
-		<p>
+		<p class="fuente">
 			1.- Si a partir día <strong>{{--{!! $datos->desaparecidoFecha !!} --}}</strong>, a la fecha de recepción del presente oficio, en alguno de los DIF municipales, casas de Protección Social, Albergues, orfanatos según sea el caso, de este Estado, existe registro sobre el  ingreso del ciudadano <strong>{{--{!! $datos->desaparecidoNombre !!} --}}</strong> o de personas que coincida con sus características físicas;
 		</p>
-		<p>
+		<p class="fuente">
 			2.- Para el caso de ser afirmativo, deberá informar el nombre y domicilio de la institución; fecha de ingreso, el motivo del ingreso y la fecha en que se retiraron de la institución.
 		</p>
-		<p>
+		<p class="fuente">
 			La información solicitada deberá ser remitida en un término no mayor a 24 horas contadas a partir de la recepción del presente oficio. Sin más por el momento aprovecho la ocasión para enviarle un cordial saludo.
 		</p>
 	</div>
@@ -140,12 +134,12 @@
 
 <div id="atentamente">
 	
-	<p align="center"><strong> ATENTAMENTE</strong></p>
-	<br>	
-	<p align="center"><strong> LIC. </strong>	<strong> {{--{!! $datos->fiscalNombre !!}--}} </strong>
+	<p align="center" class="fuente"><strong> ATENTAMENTE</strong></p>
+	
+	<p align="center" class="fuente"><strong> LIC. </strong>	<strong> {{--{!! $datos->fiscalNombre !!}--}} </strong>
 	</p>
-	<p align="center"><strong> FISCAL {{--{!! $datos->fiscalNumero !!}--}} DE LA UNIDAD INTEGRAL DE PROCURACIÓN</strong></p>
-	<p align="center"><strong> DE JUSTICIA DEL {{--{!! $datos->numDistrito !!}--}}  DISTRITO JUDICIAL DE {{--{!! $datos->fiscalLugar !!}--}}, VERACRUZ</strong></p>
+	<p align="center" class="fuente"><strong> FISCAL {{--{!! $datos->fiscalNumero !!}--}} DE LA UNIDAD INTEGRAL DE PROCURACIÓN</strong></p>
+	<p align="center" class="fuente"><strong> DE JUSTICIA DEL {{--{!! $datos->numDistrito !!}--}}  DISTRITO JUDICIAL DE {{--{!! $datos->fiscalLugar !!}--}}, VERACRUZ</strong></p>
 
 
 
@@ -154,7 +148,7 @@
 <div id="firma">
 	
 	
-	<p align="right"><strong> FD :</strong>	<strong> ASDFAS651FG5135df351g3d135df1g13f51gd1hjt35896{{--{!! $datos->fiscalNombre !!}--}} </strong>
+	<p align="right" ><strong> FD :</strong>	<strong> ASDFAS651FG5135df351g3d135df1g13f51gd1hjt35896{{--{!! $datos->firma !!}--}} </strong>
 	</p>
 	
 
