409abc00e2b3b4fae6257879435040367569934b
ADFGE\AlejandroFernandezToledo <afernandezt@fiscaliaveracruz.gob.mx>
2018-05-03 16:15:05 -0500

    actualizaciones y cambio de nombre a plantillas

--- app/Http/Controllers/CargarDocumentosController.php
+++ app/Http/Controllers/CargarDocumentosController.php
@@ -123,7 +123,7 @@ class CargarDocumentosController extends Controller
                                             ->get();*/
         
         //$desaparecido = \App\Models\Desaparecido::find($datos[0]->id);
-        $view = view('plantillas.24-albergues'/*, compact('desaparecido')*/)->render();
+        $view = view('plantillas.form_24_albergues'/*, compact('desaparecido')*/)->render();
         $pdf =\App::make('dompdf.wrapper');
         $pdf -> loadHTML($view);
         
