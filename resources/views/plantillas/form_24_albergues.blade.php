5dc706e4a04a963ff77cc1df41e16cf470c6c974
ADFGE\AlejandroFernandezToledo <afernandezt@fiscaliaveracruz.gob.mx>
2018-05-03 16:30:29 -0500

    cambios en cargos de los oficios

--- resources/views/plantillas/form_24_albergues.blade.php
+++ resources/views/plantillas/form_24_albergues.blade.php
@@ -71,7 +71,7 @@
 
 <body>
 
-<div id="fondo"></div>
+
 
 <div id="encabezado"></div>
 		
@@ -79,6 +79,9 @@
 	
 	<p class="fuente"><strong> LIC. </strong>	<strong> {{--{!! $datos->nombreRemitente !!}--}} </strong>
 	</p>
+	<p class="fuente"><strong> Director del DIF municipal</strong>
+	</p>
+	
 	
 	
 	 <p class="fuente"><strong> Xalapa, Ver. </strong>	<strong> {{--{!! $datos->fechaHoy !!}--}}  </strong></p>
@@ -100,7 +103,7 @@
 			Con fundamento en lo que disponen el artículo 19, 20 y 21 de la Constitución General de la República; 67 Fracción I apartado A de la Constitución Política Local; <strong>{{--{!! $datos->articulos !!} --}} </strong></p>
 		<br>	
 		<p align="justify" class="fuente">
-			El Protocolo Homologado para la Búsqueda de Personas Desaparecidas y la Investigación del Delito de Desaparición Forzada; los acuerdos 025/2011, 01/2013, 023/2013, emitidos por el Procurador General de Justicia del Estado; acuerdo General número 07/2017 signado por el Fiscal General del Estado de Veracruz; I, II, V, VI y XVII, de la Declaración Americana de los Derechos y Deberes del Hombre; 1, 2, 3, 7 y 8 de la Declaración Universal de Derechos Humanos; 1, 3, 6 numero 1; 9 numero 1; 27 y 26 del Pacto Internacional de Derechos Civiles y Políticos; 3 y 5 del Pacto Internacional de Derechos Económicos, Sociales y Culturales; 8, 27 núm. 1, 9, 24 y 25 de la Convención Americana sobre Derechos Humanos, “Pacto San José”; 1, 8, 12 incisos a) y b) y 27) de los Principios y directrices básicas sobre el derecho de las víctimas de violaciones manifiestas de las normas Internacionales de Derechos Humanos y de violaciones graves del derecho internacional humanitario a interponer recursos y obtener reparaciones; 1, 2 y 3 de la Declaración Sobre los Principios Fundamentales  de justicia para las víctimas del delito y del abuso de poder; así como el artículo 7 Fracción X, de la Ley General de Victimas, le solicito su colaboración para con esta Representación Social, en la búsqueda y localización y presentación del ciudadano <strong>{{--{!! $datos->desaparecidoNombre !!} --}}</strong>, quien se encuentra desaparecido, siendo visto por última vez en <strong>{{--{!! $datos->desaparecidoLugar !!} --}}</strong>, el día <strong>{{--{!! $datos->desaparecidoFecha !!} --}}</strong>, a las <strong>{{--{!! $datos->desaparecidoHora !!} --}}</strong>  horas, así como del vehículo en el que viajaba siendo un: <strong>{{--{!! $datos->vehiculoDescripcion !!} --}}</strong> para lo cual anexo al presente:
+			El Protocolo Homologado para la Búsqueda de Personas Desaparecidas y la Investigación del Delito de Desaparición Forzada; los acuerdos 025/2011, 01/2013, 023/2013, emitidos por el Procurador General de Justicia del Estado; acuerdo General número 07/2017 signado por el Fiscal General del Estado de Veracruz; I, II, V, VI y XVII, de la Declaración Americana de los Derechos y Deberes del Hombre; 1, 2, 3, 7 y 8 de la Declaración Universal de Derechos Humanos; 1, 3, 6 numero 1; 9 numero 1; 27 y 26 del Pacto Internacional de Derechos Civiles y Políticos; 3 y 5 del Pacto Internacional de Derechos Económicos, Sociales y Culturales; 8, 27 núm. 1, 9, 24 y 25 de la Convención Americana sobre Derechos Humanos, “Pacto San José”; 1, 8, 12 incisos a) y b) y 27) de los Principios y directrices básicas sobre el derecho de las víctimas de violaciones manifiestas de las normas Internacionales de Derechos Humanos y de violaciones graves del derecho internacional humanitario a interponer recursos y obtener reparaciones; 1, 2 y 3 de la Declaración Sobre los Principios Fundamentales  de justicia para las víctimas del delito y del abuso de poder; así como el artículo 7 Fracción X, de la Ley General de Victimas, le solicito su colaboración para con esta Representación Social, en la búsqueda y localización y presentación del ciudadano(a) <strong>{{--{!! $datos->desaparecidoNombre !!} --}}</strong>, quien se encuentra desaparecido, siendo visto por última vez en <strong>{{--{!! $datos->desaparecidoLugar !!} --}}</strong>, el día <strong>{{--{!! $datos->desaparecidoFecha !!} --}}</strong>, a las <strong>{{--{!! $datos->desaparecidoHora !!} --}}</strong>  horas, así como del vehículo en el que viajaba siendo un: <strong>{{--{!! $datos->vehiculoDescripcion !!} --}}</strong> para lo cual anexo al presente:
 		
 		</p>
 	</div>
@@ -112,7 +115,7 @@
 	<div id="sangria">
 
 		<p class="fuente">
-			Lo anterior a efecto de que las documentales antes señaladas sean distribuidas en los distintos DIF municipales, casas de Protección Social, Albergues, orfanatos según sea el caso, de este Estado y sean colocadas en lugares visibles al público en general, ya que estas acciones contribuyen a la búsqueda eficiente del ciudadano  <strong>{{--{!! $datos->desaparecidoNombre !!} --}}</strong>.
+			Lo anterior a efecto de que las documentales antes señaladas sean distribuidas en los distintos DIF municipales, casas de Protección Social, Albergues, orfanatos según sea el caso, de este Estado y sean colocadas en lugares visibles al público en general, ya que estas acciones contribuyen a la búsqueda eficiente del ciudadano(a)  <strong>{{--{!! $datos->desaparecidoNombre !!} --}}</strong>.
 		</p>
 
 		<p class="fuente">
@@ -120,7 +123,7 @@
 		</p>
 
 		<p class="fuente">
-			1.- Si a partir día <strong>{{--{!! $datos->desaparecidoFecha !!} --}}</strong>, a la fecha de recepción del presente oficio, en alguno de los DIF municipales, casas de Protección Social, Albergues, orfanatos según sea el caso, de este Estado, existe registro sobre el  ingreso del ciudadano <strong>{{--{!! $datos->desaparecidoNombre !!} --}}</strong> o de personas que coincida con sus características físicas;
+			1.- Si a partir día <strong>{{--{!! $datos->desaparecidoFecha !!} --}}</strong>, a la fecha de recepción del presente oficio, en alguno de los DIF municipales, casas de Protección Social, Albergues, orfanatos según sea el caso, de este Estado, existe registro sobre el  ingreso del ciudadano(a) <strong>{{--{!! $datos->desaparecidoNombre !!} --}}</strong> o de personas que coincida con sus características físicas;
 		</p>
 		<p class="fuente">
 			2.- Para el caso de ser afirmativo, deberá informar el nombre y domicilio de la institución; fecha de ingreso, el motivo del ingreso y la fecha en que se retiraron de la institución.
@@ -150,11 +153,10 @@
 	
 	<p align="right" ><strong> FD :</strong>	<strong> ASDFAS651FG5135df351g3d135df1g13f51gd1hjt35896{{--{!! $datos->firma !!}--}} </strong>
 	</p>
-	
-
-
 
 </div>
+
+
 
 
 
