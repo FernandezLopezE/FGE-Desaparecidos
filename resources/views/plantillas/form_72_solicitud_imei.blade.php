906d5b800b0d2c821a51adb947e2ee8e10ca9768
Berenice Contreras Dominguez <bnicecd@gmail.com>
2018-05-07 17:33:01 -0500

    Plantillas de más de 72 horas

---
+++ resources/views/plantillas/form_72_solicitud_imei.blade.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Documento</title>
+<style type="text/css">
+
+
+@page { margin: 1px; }
+
+		body{
+		background-image: url(./images/fondo-plantillas2.png);
+		background-size: 30%;
+		font-family: Arial;
+		
+		}
+
+		div#encabezado{
+			
+			
+			height:120px;
+			
+			
+		}
+		div#firma{
+			margin:auto;
+			margin-left: 200px;
+			margin-right: 100px;
+		
+			
+		}
+		#firma p {font-size: 5x;}
+	
+		
+		div#atentamente{
+			margin:auto;
+			margin-left: 250px;
+			margin-right: 100px;
+			width: 850px;
+			height:200px;
+			
+			
+		}
+
+		div#cuerpo{
+			margin:auto;
+			margin-left: 250px;
+			margin-right: 100px;
+			width: 800px;
+			height:1400px;
+			
+			
+		}
+
+		
+
+		
+		
+		
+		p.fuente { font-size:18px;
+				font-family: Arial, Helvetica, sans-serif; 
+		}
+
+		#sangria p {text-indent:50px ; margin:0px ; padding:0px ; border:0px}
+	
+			
+</style>
+
+</head>
+
+
+<body>
+
+
+
+<div id="encabezado"></div>
+		
+<div id="cuerpo">
+	
+	<p class="fuente"><strong> LIC. </strong>	<strong> {{--{!! $datos->nombreRemitente !!}--}} </strong>
+	</p>
+	<p class="fuente"><strong> FISCALIA DE INVESTIGACIONES MINISTERIALES
+</strong>
+	</p>
+		
+	 <p class="fuente"><strong> Xalapa, Ver. </strong>	<strong> {{--{!! $datos->fechaHoy !!}--}}  </strong></p>
+	
+	
+	
+	<p class="fuente"> <strong> No. de Inv. </strong><strong> {{--{!! $datos->numCarpeta !!} --}} </strong></p>
+	
+	
+	
+	<p class="fuente"><strong> OFICIO NO. FGE/FIM/FEADPD/</strong>	<strong> {{--{!! $datos->numOficio !!}/{!! $datos->anio !!} --}} </strong></p>
+	
+	<br>
+	<p class="fuente" align="left"><strong> PRESENTE</strong></p>	
+	
+	<div id="sangria">
+
+		<p align="justify"  class="fuente">
+			Con fundamento en lo que disponen el artículo 19, 20 y 21 de la Constitución General de la República; 67 Fracción I apartado A de la Constitución Política Local; <strong>{{--{!! $datos->articulos !!} --}} </strong></p>
+		<br>	
+		<p align="justify" class="fuente">
+			Así como los numerales 189 y 190 Fracción II de la Ley Federal de Telecomunicaciones y Radiodifusión, publicada en el Diario Oficial de la Federación en fecha catorce de Julio del dos mil catorce; I, II, V, VI y XVII, de la Declaración Americana de los Derechos y Deberes del Hombre; 1, 2, 3, 7 y 8 de la Declaración Universal de Derechos Humanos; 1, 3, 6 numero 1; 9 numero 1; 27 y 26 del Pacto Internacional de Derechos Civiles y Políticos; 3 y 5 del Pacto Internacional de Derechos Económicos, Sociales y Culturales; 8, 27 núm. 1, 9, 25 y 25 de la Convención Americana sobre Derechos Humanos, “Pacto San José”; 1, 8, 12 incisos a) y b) y 27) de e los Principios y directrices básicas sobre el derecho de las víctima de violaciones manifiestas de las normas Internacionales de Derechos Humanos y de violaciones graves del derecho internacional humanitario a interponer recursos y obtener reparaciones; 1, 2 y 3 de la Declaración Sobre los Principios Fundamentales  de justicia para las víctimas del delito y del abuso de poder; 1, 2, 4, 5, 7, Fracción I, VII y XXVI, 10, 11, 18, 21 y demás relativos aplicables de ley General de Victimas;  acuerdo 25/2011, acuerdo 11/2013  Acuerdo 23/2013, todos de la Procuraduría General de Justicia del Estado, en los cuales se establece los lineamientos para la atención de personas desaparecidas; acuerdos 02/2014 y 11/2014, signados por el Procurador General de Justicia del Estado, le solicito de la manera más atenta y respetuosa, gire oficio al C. Representante Legal de PEGASO PCS, S.A. DE C.V., con domicilio en la calle
+			Prolongación Paseo de la Reforma 1200, Piso 18, Colonia Cruz Manca, Delegación Cuajimalpa de Morelos, México, D.F., C.P. 05349., para que realice una búsqueda en los respectivos registros o bases de datos con que cuenta la referida operadora de telefonía celular (EIR) a efecto de determinar si los siguientes números de Identidad Internacional de Equipo Móvil, IMEI (International Mobile Equipment Identity). 
+		</p>
+	</div>
+	<div id="sangria">
+		<br>
+		<p align="justify" class="fuente">
+			1.- 860519022680993
+		</p>
+		<br>
+		<p align="justify" class="fuente">
+			Se encuentran registrados dentro de sus  bases de datos correspondientes, en su caso se informe si los equipos de telefonía celular a los que corresponden dichos números de IMEI: 
+		</p>
+		<p align="justify" class="fuente">
+			A)	 Se encuentra autorizados para recibir y realizar llamadas y su estatus actual. 
+		</p>
+		<br>
+		<p align="justify" class="fuente">
+			B)	Características técnicas y especificaciones de los equipos de telefonía celular a los que corresponden dichos números de IMEI. 
+		</p>
+		<br>
+		<p align="justify" class="fuente">
+			C)	Los números de teléfono celular con que se hayan utilizado dichos números de IMEI en la red celular del referido operador así como: nombre y domicilio de las personas que tenga registrados los números telefónicos que se encuentren vinculados con los citados números de IMEI, y si se encuentran activos al momento de la recepción del presente.
+		</p>
+		<br>
+		<p align="justify" class="fuente">
+			Lo anterior, toda vez que se dio inicio a la investigaciones ministerial  ____________________, por la comisión del delito de PRIVACIÓN ILEGAL DE LA LIBERTAD, en agravio de _______________________ y otros, debiendo informar lo siguiente (a partir del día ____________________________, hasta la fecha de recepción del presente).
+		</p>
+		<br>
+		<p align="justify" class="fuente">
+			Hago mención que esta autoridad se encuentra facultada para solicitar la información antes señala, de conformidad con lo que establece el artículo 303 del Código Nacional de Procedimientos Penales.
+		</p>
+		<br>
+		<p align="justify" class="fuente">
+			Sin otro particular por el momento, aprovecho la ocasión para enviarle un cordial saludo. 
+		</p>
+	</div>
+</div>
+	<br>
+
+<div id="atentamente">
+	
+	<p align="center" class="fuente"><strong> ATENTAMENTE</strong></p>
+	
+	<p align="center" class="fuente"><strong> LIC. </strong>	<strong> {{--{!! $datos->fiscalNombre !!}--}} </strong>
+	</p>
+	<p align="center" class="fuente"><strong> FISCAL {{--{!! $datos->fiscalNumero !!}--}} DE LA UNIDAD INTEGRAL DE PROCURACIÓN</strong></p>
+	<p align="center" class="fuente"><strong> DE JUSTICIA DEL {{--{!! $datos->numDistrito !!}--}}  DISTRITO JUDICIAL DE {{--{!! $datos->fiscalLugar !!}--}}, VERACRUZ</strong></p>
+
+
+
+</div>
+
+<div id="firma">
+	
+	
+	<p align="right" ><strong> FD :</strong>	<strong> ASDFAS651FG5135df351g3d135df1g13f51gd1hjt35896{{--{!! $datos->firma !!}--}} </strong>
+	</p>
+
+</div>
+
+		
+</body>
+
+</html>
