aafcada96b106c3945cfcea36866a6c605af0560
Carlos Avila Aguilar <krloz1003@gmail.com>
2018-05-07 16:34:44 -0500

    Quitando el comentario del controller ExtraviadoController al momento de almacenar

--- app/Http/Controllers/ExtraviadoController.php
+++ app/Http/Controllers/ExtraviadoController.php
@@ -113,7 +113,6 @@ class ExtraviadoController extends Controller
 	 */
 	public function store(ExtraviadoRequest $request)
 	{
-		dd($request->toArray());
 
 		$otroDocumento = ($request->input('idDocumentoIdentidad') == 9) ? $request->input('otroDocIdentidad') : null ;
 
