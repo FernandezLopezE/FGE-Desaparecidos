87727b8ab8f31ed6444cc7636d77b516e51443ba
Ruben8A <ochoa.rrub@gmail.com>
2018-05-10 15:01:04 -0500

    creando view para detalles de cabello, barba, bigote y patilla

---
+++ resources/views/descripcionfisica/show_cabello.blade.php
@@ -0,0 +1,21 @@
+<div id="detallesV">	
+	<button class="btn btn-dark pull-right" id="btnEditarC">EDITAR</button>
+	<div id="btnEditar">
+		
+	</div>
+	<div id="pCabello">
+		
+	</div>
+
+	<div id="pBarba">
+		
+	</div>
+
+	<div id="pBigote">
+		
+	</div>
+
+	<div id="pPatilla">
+		
+	</div>
+</div>
