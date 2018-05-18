09a5e01e3912d81849f9f386a6c021436cf08d5b
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-05-17 22:52:56 -0500

    agregando función para el llenado de los select para ceja primera parte

--- public/personal/js/avatar/cara.js
+++ public/personal/js/avatar/cara.js
@@ -6,4 +6,130 @@ $(document).ready(function() {
 	$("#minCara").click(function(event) {
         $("#formCara").toggle();
     });
+
+	//INICIO FUNCIONES PARA CEJAS
+	$('#tieneCabello').on('change', function(){
+		var idPartesCuerpo = 73;
+		var opcion = $(this).val();
+		alert("hola");
+		if(opcion == SÍ){
+			console.log("Entro");
+			//obtener tipo
+	    	$.ajax({
+	                url: '/descripcionfisica/get_tipos/'+idPartesCuerpo,
+	                type:"GET",
+	                dataType:"json",
+
+	                success:function(data) {
+	                        $("#tipoCeja").empty();
+	                        console.log("hecho");
+	                    $.each(data, function(key, value){                        
+
+	                        $("#tipoCeja").append('<option value="'+ value.id +'">' +  value.nombre + '</option>');
+
+	                    });
+
+	                },
+	                
+	        });
+
+	        //obtener particularidades
+	        $.ajax({
+	                url: '/descripcionfisica/get_particularidades/'+idPartesCuerpo,
+	                type:"GET",
+	                dataType:"json",
+
+	                success:function(data) {
+	                        $("#idSubParticularidades").empty();
+	                    $.each(data, function(key, value){                        
+
+	                        $("#idSubParticularidades").append('<option value="'+ value.id +'">' +  value.nombre + '</option>');
+
+	                    });
+
+	                },
+	                
+	        });
+
+	        //obtener modificaciones
+	        $.ajax({
+	                url: '/descripcionfisica/get_modificaciones/'+idPartesCuerpo,
+	                type:"GET",
+	                dataType:"json",
+
+	                success:function(data) {
+	                        $("#idSubModificaciones").empty();
+	                    $.each(data, function(key, value){                        
+
+	                        $("#idSubModificaciones").append('<option value="'+ value.id +'">' +  value.nombre + '</option>');
+
+	                    });
+
+	                },
+	                
+	            });
+		}
+		
+	});
+
+    $("#posCejas").on('change', function(){
+    	var idPartesCuerpo = $(this).val();
+    	//obtener tipo
+    	$.ajax({
+                url: '/descripcionfisica/get_tipos/'+idPartesCuerpo,
+                type:"GET",
+                dataType:"json",
+
+                success:function(data) {
+                        $("#tipoCeja").empty();
+                        console.log("hecho");
+                    $.each(data, function(key, value){                        
+
+                        $("#tipoCeja").append('<option value="'+ value.id +'">' +  value.nombre + '</option>');
+
+                    });
+
+                },
+                
+        });
+
+        //obtener particularidades
+        $.ajax({
+                url: '/descripcionfisica/get_particularidades/'+idPartesCuerpo,
+                type:"GET",
+                dataType:"json",
+
+                success:function(data) {
+                        $("#idSubParticularidades").empty();
+                    $.each(data, function(key, value){                        
+
+                        $("#idSubParticularidades").append('<option value="'+ value.id +'">' +  value.nombre + '</option>');
+
+                    });
+
+                },
+                
+        });
+
+        //obtener modificaciones
+        $.ajax({
+                url: '/descripcionfisica/get_modificaciones/'+idPartesCuerpo,
+                type:"GET",
+                dataType:"json",
+
+                success:function(data) {
+                        $("#idSubModificaciones").empty();
+                    $.each(data, function(key, value){                        
+
+                        $("#idSubModificaciones").append('<option value="'+ value.id +'">' +  value.nombre + '</option>');
+
+                    });
+
+                },
+                
+            });
+
+    });
+
+    //FIN FUNCIONES PARA CEJAS
 });
