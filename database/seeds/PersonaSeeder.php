f5fa99ff6cf522f22258b523ceec01e7a87d7d25
Berenice Contreras Dominguez <bnicecd@gmail.com>
2018-05-17 22:49:54 -0500

    Commit de personaseeder para el poblado de datos

--- database/seeds/PersonaSeeder.php
+++ database/seeds/PersonaSeeder.php
@@ -22,7 +22,7 @@ class PersonaSeeder extends Seeder
         //factory(\App\Models\Plan::class, 3)->create()->each(function ($planes) {
         //planes->auditorias()->save(factory(\App\Models\Auditoria::class)->make());
 
-        factory(App\Models\Cedula::class, 5)->create()->each(function ($cedula){
+        factory(App\Models\Cedula::class, 10)->create()->each(function ($cedula){
         $cedula->desaparecidos()->save(factory(App\Models\Desaparecido::class)->make());
         });
 
@@ -30,7 +30,10 @@ class PersonaSeeder extends Seeder
         //$persona->desaparecidos()->save(factory(App\Models\Desaparecido::class)->make());
         //});
 
-        factory(App\Models\Desaparecido::class, 5)->create();
+        factory(App\Models\Desaparecido::class, 10)->create()->each(function ($desaparecido){
+        $desaparecido->domicilios()->save(factory(App\Models\Domicilio::class)->make());
+        });
+
 
 
     }
