1a367da3a13f9850831cd39ddd4b934e3cb5ee26
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-05-17 19:33:22 -0500

    solucionado conflicto al ocultar detalles de bigote y patilla

--- resources/views/descripcionfisica/show_cabello.blade.php
+++ resources/views/descripcionfisica/show_cabello.blade.php
@@ -1,21 +1,19 @@
-<div class="form-group row">
-	<div class="col">
-		
+<div id="detallesV">
+
+	<div class="form-group row" >
+		<div class="col" id="pCabello">
+			
+		</div>
+		<div class="col" id="pBarba">
+			
+		</div>
 	</div>
-</div>
-<div class="form-group row" id="detallesV">
-	<div class="col" id="pCabello">
-		
-	</div>
-	<div class="col" id="pBarba">
-		
-	</div>
-</div>
-<div class="form-group row">
-	<div class="col" id="pBigote">
-		
-	</div>
-	<div class="col" id="pPatilla">
-		
+	<div class="form-group row">
+		<div class="col" id="pBigote">
+			
+		</div>
+		<div class="col" id="pPatilla">
+			
+		</div>
 	</div>
 </div>
