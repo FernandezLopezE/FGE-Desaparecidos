9f1d3582752d9647f6a0218afae4ab916559cf19
ADFGE\dlanda <proyectosmx94@gmail.com>
2018-05-17 22:53:07 -0500

    FrontEnd de sección "Cara"

--- resources/views/descripcionfisica/seccion_Cara.blade.php
+++ resources/views/descripcionfisica/seccion_Cara.blade.php
@@ -35,13 +35,220 @@
 		</div>
 	</div><br>
 
-	<div class="row">
+	<div class="form-group row">
 		<div class="col">
 			{!! Form::textarea('observacionesCabello', '', ['class' => 'form-control', 'id' => 'observacionesCabello', 'rows' => '1', 'placeholder' => 'Observaciones'] ) !!}
 		</div>
+	</div><hr>
+
+	<!--Sección ojos-->
+	<div class="form-group row">
+		<div class="col-6">
+			{!! Form::label ('infoOjos','¿Tiene información de los ojos?') !!}
+			{!! Form::select('infoOjos', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'infoOjos'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('posCejas','Posición') !!}
+			{!! Form::select('posCejas',  $cejasParte, '', ['class' => 'form-control', 'id' => 'posCejas'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('tipoCeja','Color') !!}
+			{!! Form::select('tipoCeja', $tipoCeja, '', ['class' => 'form-control', 'id' => 'tipoCeja'] ) !!}
+		</div>
 	</div>
+
+	<div class="form-group row">
+		<div class="col-3">
+			{!! Form::label ('colorCejas','Tamaño') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+		<div class="col-3">
+			{!! Form::label ('colorCejas','Particularidades') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('colorCejas','Modificaciones') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+	</div>
+
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::textarea('observacionesCabello', '', ['class' => 'form-control', 'id' => 'observacionesCabello', 'rows' => '1', 'placeholder' => 'Observaciones'] ) !!}
+		</div>
+	</div><hr>	
+
+	<!--Sección nariz-->	
+	<div class="form-group row">
+		<div class="col-6">
+			{!! Form::label ('infoOjos','¿Tiene información de la nariz?') !!}
+			{!! Form::select('infoOjos', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'infoOjos'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('tipoCeja','Tipo') !!}
+			{!! Form::select('tipoCeja', $tipoCeja, '', ['class' => 'form-control', 'id' => 'tipoCeja'] ) !!}
+		</div>
+	</div>
+	
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::label ('colorCejas','Particularidades') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('colorCejas','Modificaciones') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+	</div>
+		
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::textarea('observacionesCabello', '', ['class' => 'form-control', 'id' => 'observacionesCabello', 'rows' => '1', 'placeholder' => 'Observaciones'] ) !!}
+		</div>
+	</div><hr>	
+
+	<!--Sección boca-->
+	<div class="form-group row">
+		<div class="col-6">
+			{!! Form::label ('infoOjos','¿Tiene información de la boca?') !!}
+			{!! Form::select('infoOjos', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'infoOjos'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('tipoCeja','Tamaño') !!}
+			{!! Form::select('tipoCeja', $tipoCeja, '', ['class' => 'form-control', 'id' => 'tipoCeja'] ) !!}
+		</div>
+	</div>
+	
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::label ('colorCejas','Particularidades') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('colorCejas','Modificaciones') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+	</div>
+		
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::textarea('observacionesCabello', '', ['class' => 'form-control', 'id' => 'observacionesCabello', 'rows' => '1', 'placeholder' => 'Observaciones'] ) !!}
+		</div>
+	</div><hr>	
+
+	<!--Sección orejas-->
+	<div class="form-group row">
+		<div class="col-6">
+			{!! Form::label ('infoOjos','¿Tiene información de las orejas?') !!}
+			{!! Form::select('infoOjos', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'infoOjos'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('posCejas','Posición') !!}
+			{!! Form::select('posCejas',  $cejasParte, '', ['class' => 'form-control', 'id' => 'posCejas'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('tipoCeja','Tipo') !!}
+			{!! Form::select('tipoCeja', $tipoCeja, '', ['class' => 'form-control', 'id' => 'tipoCeja'] ) !!}
+		</div>
+	</div>
+
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::label ('colorCejas','Particularidades') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('colorCejas','Modificaciones') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+	</div>
+
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::textarea('observacionesCabello', '', ['class' => 'form-control', 'id' => 'observacionesCabello', 'rows' => '1', 'placeholder' => 'Observaciones'] ) !!}
+		</div>
+	</div><hr>
+
+	<!--Sección frente-->
+	<div class="form-group row">
+		<div class="col-6">
+			{!! Form::label ('infoOjos','¿Tiene información de la frente?') !!}
+			{!! Form::select('infoOjos', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'infoOjos'] ) !!}
+		</div>
+		
+	</div>
+	
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::label ('colorCejas','Particularidades') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('colorCejas','Modificaciones') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+	</div>
+		
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::textarea('observacionesCabello', '', ['class' => 'form-control', 'id' => 'observacionesCabello', 'rows' => '1', 'placeholder' => 'Observaciones'] ) !!}
+		</div>
+	</div><hr>	
+
+	<!--Sección mejillas-->
+	<div class="form-group row">
+		<div class="col-6">
+			{!! Form::label ('infoOjos','¿Tiene información de los cachetes?') !!}
+			{!! Form::select('infoOjos', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'infoOjos'] ) !!}
+		</div>
+		
+	</div>
+	
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::label ('colorCejas','Particularidades') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('colorCejas','Modificaciones') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+	</div>
+		
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::textarea('observacionesCabello', '', ['class' => 'form-control', 'id' => 'observacionesCabello', 'rows' => '1', 'placeholder' => 'Observaciones'] ) !!}
+		</div>
+	</div><hr>
+
+	<!--Sección menton-->
+	<div class="form-group row">
+		<div class="col-6">
+			{!! Form::label ('infoOjos','¿Tiene información del mentón?') !!}
+			{!! Form::select('infoOjos', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'infoOjos'] ) !!}
+		</div>
+		
+	</div>
+	
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::label ('colorCejas','Particularidades') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+		<div class="col">
+			{!! Form::label ('colorCejas','Modificaciones') !!}
+			{!! Form::select('colorCejas', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SÍ' => 'SÍ', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'colorCejas'] ) !!}
+		</div>
+	</div>
+		
+	<div class="form-group row">
+		<div class="col">
+			{!! Form::textarea('observacionesCabello', '', ['class' => 'form-control', 'id' => 'observacionesCabello', 'rows' => '1', 'placeholder' => 'Observaciones'] ) !!}
+		</div>
+	</div>	
 </div>
 </form>
+
 
 
 
