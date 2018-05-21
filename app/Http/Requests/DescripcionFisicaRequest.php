06c47eda205fda14527ff30f47430c1c816203e4
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-05-21 16:51:23 -0500

    Modificando parametro de estatura minima a 40 centimetros

--- app/Http/Requests/DescripcionFisicaRequest.php
+++ app/Http/Requests/DescripcionFisicaRequest.php
@@ -26,7 +26,7 @@ class DescripcionFisicaRequest extends FormRequest
         return [
             //
 
-            'estatura' => 'int|max:250|min:50',
+            'estatura' => 'int|max:250|min:40',
             'peso' => 'numeric|min:2.400|max:635.000',
             
         ];
@@ -35,7 +35,7 @@ class DescripcionFisicaRequest extends FormRequest
     public function messages()
     {
         return [
-            'estatura.min' => 'La estatura minima debe ser de al menos 50 cm.',
+            'estatura.min' => 'La estatura minima debe ser de al menos 40 cm.',
             'estatura.max' => 'La estatura maxima no debe ser mayor a 250 cm.',
             'peso.min' => 'El peso minimo debe debe ser 2.400 kg.',
             'peso.max' => 'El peso máximo no debe ser mayor a 635 kg.'
