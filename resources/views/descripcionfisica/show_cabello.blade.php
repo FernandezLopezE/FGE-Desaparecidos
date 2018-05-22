563612413344478f88a8cf7d04bfa1e6b5b6e45c
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-05-21 21:09:51 -0500

    agregando estilos al show para que se oculten

--- resources/views/descripcionfisica/show_cabello.blade.php
+++ resources/views/descripcionfisica/show_cabello.blade.php
@@ -1,18 +1,18 @@
 <div id="detallesV">
 
 	<div class="form-group row" >
-		<div class="col" id="pCabello">
+		<div class="col" id="pCabello" style="display: none;">
 			
 		</div>
-		<div class="col" id="pBarba">
+		<div class="col" id="pBarba" style="display: none;">
 			
 		</div>
 	</div>
 	<div class="form-group row">
-		<div class="col" id="pBigote">
+		<div class="col" id="pBigote" style="display: none;">
 			
 		</div>
-		<div class="col" id="pPatilla">
+		<div class="col" id="pPatilla" style="display: none;">
 			
 		</div>
 	</div>
