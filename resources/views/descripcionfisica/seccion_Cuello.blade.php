354368615da0bd908232ed5ac9937cbd60bbd34f
ADFGE\dlanda <proyectosmx94@gmail.com>
2018-05-21 21:06:57 -0500

    xD

---
+++ resources/views/descripcionfisica/seccion_Cuello.blade.php
@@ -0,0 +1,15 @@
+<form id="formCuello">
+<div class="card-header">
+	<h5 class="card-title">Rostro
+		<i class="fa fa-pencil pull-right" id="btnEditarC"></i>
+		<i class="fa fa-times-circle" style="float: right; margin-left: 10px;" id="cerrar"></i>
+		<i class="fa fa-minus-square" style="float: right;" id="minCara"></i>
+	</h5>
+</div>
+
+<div class="card-body">
+	<h1>Hola mundo</h1>
+</div> 
+</form>
+
+<script src="personal/js/avatar/cuello-pecho.js"></script>
