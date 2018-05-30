d05ed2e9d698165075458c45d63750eaf56c055c
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-05-29 22:03:52 -0500

    cambiando id de las partes cuerpo

--- database/seeds/CatTamanoCuerpoSeeder.php
+++ database/seeds/CatTamanoCuerpoSeeder.php
@@ -14,51 +14,28 @@ class CatTamanoCuerpoSeeder extends Seeder
         //
         DB::table('cat_tamano_cuerpo')->insert([
         	//CABELLO
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '55'],
-            [ 'nombre' => 'CORTO', 'idPartesCuerpo' => '55'],
-            [ 'nombre' => 'MEDIANO AL HOMBRO', 'idPartesCuerpo' => '55'],
-            [ 'nombre' => 'LARGO', 'idPartesCuerpo' => '55'],
-            [ 'nombre' => 'RAPADO', 'idPartesCuerpo' => '55'],
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '2'],
+            [ 'nombre' => 'CORTO', 'idPartesCuerpo' => '2'],
+            [ 'nombre' => 'MEDIANO AL HOMBRO', 'idPartesCuerpo' => '2'],
+            [ 'nombre' => 'LARGO', 'idPartesCuerpo' => '2'],
+            [ 'nombre' => 'RAPADO', 'idPartesCuerpo' => '2'],
 
             //OJO IZQUIERDO
-			[ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '37'],
-            [ 'nombre' => 'GRANDE', 'idPartesCuerpo' => '37'],
-            [ 'nombre' => 'MEDIANO', 'idPartesCuerpo' => '37'],
-            [ 'nombre' => 'PEQUEÑO', 'idPartesCuerpo' => '37'],
+			[ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '9'],
+            [ 'nombre' => 'GRANDE', 'idPartesCuerpo' => '9'],
+            [ 'nombre' => 'MEDIANO', 'idPartesCuerpo' => '9'],
+            [ 'nombre' => 'PEQUEÑO', 'idPartesCuerpo' => '9'],
 
-            //OJO DERECHO
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '36'],
-            [ 'nombre' => 'GRANDE', 'idPartesCuerpo' => '36'],
-            [ 'nombre' => 'MEDIANO', 'idPartesCuerpo' => '36'],
-            [ 'nombre' => 'PEQUEÑO', 'idPartesCuerpo' => '36'],
+            
 
-            //AMBOS OJOS
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '74'],
-            [ 'nombre' => 'GRANDE', 'idPartesCuerpo' => '74'],
-            [ 'nombre' => 'MEDIANO', 'idPartesCuerpo' => '74'],
-            [ 'nombre' => 'PEQUEÑO', 'idPartesCuerpo' => '74'],
+            //boca
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '13'],
+            [ 'nombre' => 'GRANDE', 'idPartesCuerpo' => '13'],
+            [ 'nombre' => 'GRANDE Y ALARGADA', 'idPartesCuerpo' => '13'],
+            [ 'nombre' => 'MEDIANA', 'idPartesCuerpo' => '13'],
+            [ 'nombre' => 'PEQUEÑA', 'idPartesCuerpo' => '13'],
 
-            //AMBOS LABIOS
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '72'],
-            [ 'nombre' => 'GRANDE', 'idPartesCuerpo' => '72'],
-            [ 'nombre' => 'GRANDE Y ALARGADA', 'idPartesCuerpo' => '72'],
-            [ 'nombre' => 'MEDIANA', 'idPartesCuerpo' => '72'],
-            [ 'nombre' => 'PEQUEÑA', 'idPartesCuerpo' => '72'],
-
-            //LABIO INFERIOR
-
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '19'],
-            [ 'nombre' => 'GRANDE', 'idPartesCuerpo' => '19'],
-            [ 'nombre' => 'GRANDE Y ALARGADA', 'idPartesCuerpo' => '19'],
-            [ 'nombre' => 'MEDIANA', 'idPartesCuerpo' => '19'],
-            [ 'nombre' => 'PEQUEÑA', 'idPartesCuerpo' => '19'],
-			            
-            //LABIO SUPERIOR
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '20'],
-            [ 'nombre' => 'GRANDE', 'idPartesCuerpo' => '20'],
-            [ 'nombre' => 'GRANDE Y ALARGADA', 'idPartesCuerpo' => '20'],
-            [ 'nombre' => 'MEDIANA', 'idPartesCuerpo' => '20'],
-            [ 'nombre' => 'PEQUEÑA', 'idPartesCuerpo' => '20'],
+            
         ]);
     }
 }
--- database/seeds/CatTiposCuerpoSeeder.php
+++ database/seeds/CatTiposCuerpoSeeder.php
@@ -14,124 +14,84 @@ class CatTiposCuerpoSeeder extends Seeder
         //
          DB::table('cat_tipos_cuerpo')->insert([
         	//CABELLO
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '55'],
-            [ 'nombre' => 'LACIO', 'idPartesCuerpo' => '55'],
-            [ 'nombre' => 'SEMIONDULADO', 'idPartesCuerpo' => '55'],
-            [ 'nombre' => 'ONDULADO', 'idPartesCuerpo' => '55'],
-            [ 'nombre' => 'CHINO/RIZADO', 'idPartesCuerpo' => '55'],
-            [ 'nombre' => 'CRESPO', 'idPartesCuerpo' => '55'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '55'],
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '2'],
+            [ 'nombre' => 'LACIO', 'idPartesCuerpo' => '2'],
+            [ 'nombre' => 'SEMIONDULADO', 'idPartesCuerpo' => '2'],
+            [ 'nombre' => 'ONDULADO', 'idPartesCuerpo' => '2'],
+            [ 'nombre' => 'CHINO/RIZADO', 'idPartesCuerpo' => '2'],
+            [ 'nombre' => 'CRESPO', 'idPartesCuerpo' => '2'],
+            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '2'],
 
 
             //CEJA IZQUIERDA
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '6'],
-            [ 'nombre' => 'POBLADA', 'idPartesCuerpo' => '6'],
-            [ 'nombre' => 'REGULAR', 'idPartesCuerpo' => '6'],
-            [ 'nombre' => 'ESCASA', 'idPartesCuerpo' => '6'],
-            [ 'nombre' => 'DEPILADA', 'idPartesCuerpo' => '6'],
-            [ 'nombre' => 'RASURADA', 'idPartesCuerpo' => '6'],
-            [ 'nombre' => 'SIN CEJA', 'idPartesCuerpo' => '6'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '6'],
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '8'],
+            [ 'nombre' => 'POBLADA', 'idPartesCuerpo' => '8'],
+            [ 'nombre' => 'REGULAR', 'idPartesCuerpo' => '8'],
+            [ 'nombre' => 'ESCASA', 'idPartesCuerpo' => '8'],
+            [ 'nombre' => 'DEPILADA', 'idPartesCuerpo' => '8'],
+            [ 'nombre' => 'RASURADA', 'idPartesCuerpo' => '8'],
+            [ 'nombre' => 'SIN CEJA', 'idPartesCuerpo' => '8'],
+            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '8'],
 
-            //CEJA DERECHA
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '7'],
-            [ 'nombre' => 'POBLADA', 'idPartesCuerpo' => '7'],
-            [ 'nombre' => 'REGULAR', 'idPartesCuerpo' => '7'],
-            [ 'nombre' => 'ESCASA', 'idPartesCuerpo' => '7'],
-            [ 'nombre' => 'DEPILADA', 'idPartesCuerpo' => '7'],
-            [ 'nombre' => 'RASURADA', 'idPartesCuerpo' => '7'],
-            [ 'nombre' => 'SIN CEJA', 'idPartesCuerpo' => '7'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '7'],
-
-            //AMBAS CEJAS
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '73'],
-            [ 'nombre' => 'POBLADA', 'idPartesCuerpo' => '73'],
-            [ 'nombre' => 'REGULAR', 'idPartesCuerpo' => '73'],
-            [ 'nombre' => 'ESCASA', 'idPartesCuerpo' => '73'],
-            [ 'nombre' => 'DEPILADA', 'idPartesCuerpo' => '73'],
-            [ 'nombre' => 'RASURADA', 'idPartesCuerpo' => '73'],
-            [ 'nombre' => 'SIN CEJA', 'idPartesCuerpo' => '73'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '73'],
+            
 
             //NARIZ
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '31'],
-            [ 'nombre' => 'CHATA/BOTÓN', 'idPartesCuerpo' => '31'],
-            [ 'nombre' => 'AGUILEÑA', 'idPartesCuerpo' => '31'],
-            [ 'nombre' => 'RESPINGADA/RECATA', 'idPartesCuerpo' => '31'],
-            [ 'nombre' => 'OBLITERADA (OBSTRUIDA/CERRADA)', 'idPartesCuerpo' => '31'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '31'],
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '10'],
+            [ 'nombre' => 'CHATA/BOTÓN', 'idPartesCuerpo' => '10'],
+            [ 'nombre' => 'AGUILEÑA', 'idPartesCuerpo' => '10'],
+            [ 'nombre' => 'RESPINGADA/RECATA', 'idPartesCuerpo' => '10'],
+            [ 'nombre' => 'OBLITERADA (OBSTRUIDA/CERRADA)', 'idPartesCuerpo' => '10'],
+            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '10'],
 
             //OREJA IZQUIERDA
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '35'],
-            [ 'nombre' => 'COMPLETA', 'idPartesCuerpo' => '35'],
-            [ 'nombre' => 'INCOMPLETA', 'idPartesCuerpo' => '35'],
-            [ 'nombre' => 'OBLITERADA (OBSTRUIDA/CERRADA)', 'idPartesCuerpo' => '35'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '35'],
-
-
-            //OREJA DERECHA
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '34'],
-            [ 'nombre' => 'COMPLETA', 'idPartesCuerpo' => '34'],
-            [ 'nombre' => 'INCOMPLETA', 'idPartesCuerpo' => '34'],
-            [ 'nombre' => 'OBLITERADA (OBSTRUIDA/CERRADA)', 'idPartesCuerpo' => '34'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '34'],
-
-            //AMBAS OREJAS
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '77'],
-            [ 'nombre' => 'COMPLETA', 'idPartesCuerpo' => '77'],
-            [ 'nombre' => 'INCOMPLETA', 'idPartesCuerpo' => '77'],
-            [ 'nombre' => 'OBLITERADA (OBSTRUIDA/CERRADA)', 'idPartesCuerpo' => '77'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '77'],
-
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '11'],
+            [ 'nombre' => 'COMPLETA', 'idPartesCuerpo' => '11'],
+            [ 'nombre' => 'INCOMPLETA', 'idPartesCuerpo' => '11'],
+            [ 'nombre' => 'OBLITERADA (OBSTRUIDA/CERRADA)', 'idPartesCuerpo' => '11'],
+            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '11'],
 
 
             //BARBA
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '56'],
-            [ 'nombre' => 'POBLADA', 'idPartesCuerpo' => '56'],
-            [ 'nombre' => 'ESCASA', 'idPartesCuerpo' => '56'],
-            [ 'nombre' => 'LAMPIÑO', 'idPartesCuerpo' => '56'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '56'],
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '3'],
+            [ 'nombre' => 'POBLADA', 'idPartesCuerpo' => '3'],
+            [ 'nombre' => 'ESCASA', 'idPartesCuerpo' => '3'],
+            [ 'nombre' => 'LAMPIÑO', 'idPartesCuerpo' => '3'],
+            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '3'],
 
 
             //BIGOTE
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '57'],
-            [ 'nombre' => 'POBLADA', 'idPartesCuerpo' => '57'],
-            [ 'nombre' => 'ESCASA', 'idPartesCuerpo' => '57'],
-            [ 'nombre' => 'LAMPIÑO', 'idPartesCuerpo' => '57'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '57'],
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '4'],
+            [ 'nombre' => 'POBLADA', 'idPartesCuerpo' => '4'],
+            [ 'nombre' => 'ESCASA', 'idPartesCuerpo' => '4'],
+            [ 'nombre' => 'LAMPIÑO', 'idPartesCuerpo' => '4'],
+            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '4'],
 
 
             //PATILLAS
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '58'],
-            [ 'nombre' => 'POBLADA', 'idPartesCuerpo' => '58'],
-            [ 'nombre' => 'ESCASA', 'idPartesCuerpo' => '58'],
-            [ 'nombre' => 'LAMPIÑO', 'idPartesCuerpo' => '58'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '58'],
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '5'],
+            [ 'nombre' => 'POBLADA', 'idPartesCuerpo' => '5'],
+            [ 'nombre' => 'ESCASA', 'idPartesCuerpo' => '5'],
+            [ 'nombre' => 'LAMPIÑO', 'idPartesCuerpo' => '5'],
+            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '5'],
 
             //MENTÓN
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '24'],
-            [ 'nombre' => 'SALIENTE', 'idPartesCuerpo' => '24'],
-            [ 'nombre' => 'RECTO', 'idPartesCuerpo' => '24'],
-            [ 'nombre' => 'RETRAÍDO', 'idPartesCuerpo' => '24'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '24'],
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '15'],
+            [ 'nombre' => 'SALIENTE', 'idPartesCuerpo' => '15'],
+            [ 'nombre' => 'RECTO', 'idPartesCuerpo' => '15'],
+            [ 'nombre' => 'RETRAÍDO', 'idPartesCuerpo' => '15'],
+            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '15'],
 
 
 
             //LABIOS INFERIOR
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '19'],
-            [ 'nombre' => 'DELGADO', 'idPartesCuerpo' => '19'],
-            [ 'nombre' => 'MEDIANO', 'idPartesCuerpo' => '19'],
-            [ 'nombre' => 'GRUESO', 'idPartesCuerpo' => '19'],
-            [ 'nombre' => 'MIXTO', 'idPartesCuerpo' => '19'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '19'],
+            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '14'],
+            [ 'nombre' => 'DELGADO', 'idPartesCuerpo' => '14'],
+            [ 'nombre' => 'MEDIANO', 'idPartesCuerpo' => '14'],
+            [ 'nombre' => 'GRUESO', 'idPartesCuerpo' => '14'],
+            [ 'nombre' => 'MIXTO', 'idPartesCuerpo' => '14'],
+            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '14'],
 
-            //LABIOS DERECHO
-            [ 'nombre' => 'SIN INFORMACION', 'idPartesCuerpo' => '20'],
-            [ 'nombre' => 'DELGADO', 'idPartesCuerpo' => '20'],
-            [ 'nombre' => 'MEDIANO', 'idPartesCuerpo' => '20'],
-            [ 'nombre' => 'GRUESO', 'idPartesCuerpo' => '20'],
-            [ 'nombre' => 'MIXTO', 'idPartesCuerpo' => '20'],
-            [ 'nombre' => 'OTRO', 'idPartesCuerpo' => '20'],
+            
 
 
 
