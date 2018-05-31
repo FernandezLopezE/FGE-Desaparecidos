42f4980932639aaf8b43a7a59dddd3c43496c9ab
Carlos Avila Aguilar <krloz1003@gmail.com>
2018-05-30 21:08:20 -0500

    Agregabdo el IdCarpeta de UIPJ en migracion y modelo, creando el metodo para obtener los datos de uipj y comentando los select2 de domicilio.

--- app/Http/Controllers/ConexionUipjController.php
+++ app/Http/Controllers/ConexionUipjController.php
@@ -7,115 +7,112 @@ use App\Models\Cedula;
 
 class ConexionUipjController extends Controller
 {
-    public function resolviendo_peticion($carpeta = 2)
+    public function resolviendo_peticion($idCarpeta = null)
     {
-        $carpeta = \DB::connection('mysql2')
-                            ->select('select u.nombres, u.apellidos, n.nombre as unidad, c.numCarpeta
-                                        from carpeta c
-                                        left outer join users u on c.idFiscal = u.id
-                                        left outer join unidad n on c.idUnidad = n.id
-                                        where c.id ='.$carpeta);
+        $existe = \App\Models\Cedula::where('idCarpeta', $idCarpeta)->get();
+
+        if(count($existe)){
+            $existe = $existe[0];
+            return redirect()->action(
+                'CedulaController@show', ['id' => $existe->id]
+            );
+        }
+
+        \DB::beginTransaction();
+        try {
         
+            $carpeta = \DB::connection('mysql2')
+                                ->select('select c.id, u.nombres, u.apellidos, n.nombre as unidad, c.numCarpeta
+                                            from carpeta c
+                                            left outer join users u on c.idFiscal = u.id
+                                            left outer join unidad n on c.idUnidad = n.id
+                                            where c.id ='.$idCarpeta);
+            
+            
+            //dd($carpeta[0]->id);
+            $cedula = \App\Models\Cedula::create([
+                'entrevistadorNombres' 			=> $carpeta[0]->nombres,
+                'entrevistadorPrimerAp' 		=> $carpeta[0]->apellidos,
+                'entrevistadorSegundoAp'		=> '.',
+                'entrevistadorCargo'			=> $carpeta[0]->unidad,
+                'carpeta'                       => $carpeta[0]->numCarpeta,
+                'idCarpeta'                     => $carpeta[0]->id,
+                'entrevistadorPrimeraVez'		=> 'SI',			
+                'idDialecto' 					=> '1'
+            ]);       
 
-        $carpeta = $carpeta[0];       
+            $informante = \DB::connection('mysql2')
+                        ->select('select s.nombres, s.primerAp, s.segundoAp, s.fechaNacimiento, s.rfc,
+                        s.curp, s.idNacionalidad, s.sexo,
+                        v.docIdentificacion, v.numDocIdentificacion, v.edad, v.idEstadoCivil,
+                        d.calle, d.numExterno, d.numInterno, d.idMunicipio, d.idLocalidad, d.idColonia, d.idColonia as idCp,
+                        v.telefono                    
+                        from variables_persona v
+                        left outer join extra_denunciante x on v.id = x.idVariablesPersona
+                        left outer join persona s on v.idPersona = s.id
+                        left outer join domicilio d on v.idDomicilio = d.id
+                        where v.idCarpeta='.$idCarpeta);
+            
+            $informante = $informante[0];
+            //dd($informante->segundoAp);
+            $persona = \App\Models\Persona::create([
+                'nombres'           => $informante->nombres,
+                'primerAp'          => $informante->primerAp,
+                'segundoAp'         => $informante->segundoAp,
+                'fechaNacimiento'   => \Carbon\Carbon::parse($informante->fechaNacimiento)->format('d/m/Y'),
+                'sexo'              => $informante->sexo,
+                'idNacionalidad'    => $informante->idNacionalidad,
+                'curp'              => $informante->curp
+            ]);
 
-		$cedula = Cedula::create([
-			'entrevistadorNombres' 			=> $carpeta->nombres,
-			'entrevistadorPrimerAp' 		=> $carpeta->apellidos,
-			'entrevistadorSegundoAp'		=> '.',
-            'entrevistadorCargo'			=> $carpeta->unidad,
-            'carpeta'                       => $carpeta->numCarpeta,
-			'entrevistadorPrimeraVez'		=> 'SI',			
-			'idDialecto' 					=> '1'
-		]);
-        dd($cedula);
+            $idDocumentoIdentidad = \App\Models\CatDocumento::where('nombre',$informante->docIdentificacion)->get();
+            $idDocumentoIdentidad = (count($idDocumentoIdentidad)) ? $idDocumentoIdentidad[0]->id : 1 ;       
+  
+            $telefonos[] = array('tipoTelefono' => 'CELULAR',
+                                    'lada' => '(+52)-',
+                                    'telefono' => $informante->telefono,
+                                    'ext' => null);
 
-
-
-       /* $persona = \App\Models\Persona::create([
-            'nombres'           => $carpeta->nombres,
-            'primerAp'          => $carpeta->apellidos,
-            'segundoAp'         => '.',
-            'fechaNacimiento'   => \Carbon\Carbon::parse($value)->format('d/m/Y');,
-            'sexo'              => $request->input('sexo'),
-            'idNacionalidad'    => $request->input('idNacionalidad'),
-            'curp'              => $request->input('curp'),
-            'idEstadoOrigen'    => $request->input('idEstadoOrigen'),
-        ]);
-
-        $desaparecido = \App\Models\Desaparecido::create([
-            'idCedula'                  => $request->input('idCedula'),
-            'idPersona'                 => $persona->id,
-            'apodo'                     => $request->input('apodo'),
-            'edadAparente'              => $request->input('edadAparente'),
-            'edadExtravio'              => $request->input('edadExtravio'),
-            'embarazo'                  => $embarazo,
-            'numGestacion'              => $numGestacion,
-            'tipoGestacion'             => $tipoGestacion,
-            'rumoresBebe'               => $rumoresBebe,
-            'pormenores'                => $pormenores,
-            'antecedentesJudiciales'    => $request->input('antecedentesJudiciales'),
-            'otroDocIdentidad'          => $otroDocumento,
-            'numDocIdentidad'           => $request->input('numDocIdentidad'),
-            'idEdocivil'                => $request->input('idEdocivil'),
-            'idOcupacion'               => $request->input('idOcupacion'),
-            'idEscolaridad'             => $request->input('idEscolaridad'),
-            'idDocumentoIdentidad'      => $request->input('idDocumentoIdentidad'),
-            'tipoPersona'               => 'DESAPARECIDA',
-        ]);        
+            $desaparecido = \App\Models\Desaparecido::create([
+                'idPersona'             => $persona->id,
+                'idCedula'              => $cedula->id,
+                'idParentesco'          => '1',
+                'idDocumentoIdentidad'  => $idDocumentoIdentidad, 
+                'otroDocIdentidad'      => null,
+                'numDocIdentidad'       => $informante->numDocIdentificacion,
+                'correoElectronico'     => null,
+                'telefonos'          	=> json_encode($telefonos),
+                'informante'            => 1,
+                'notificaciones'        => 1,
+                'tipoPersona'           => 'INFORMANTE',
+            ]);
+            
+            $domicilio = \App\Models\Domicilio::create([
+                'idDesaparecido'    => $desaparecido->id,
+                'tipoDireccion'     => 'PERSONAL',
+                'calle'             => $informante->calle,
+                'numExterno'        => $informante->numExterno,
+                'numInterno'        => $informante->numInterno,
+                'idestado'          => 30,
+                'idMunicipio'       => $informante->idMunicipio,
+                'idLocalidad'       => $informante->idLocalidad,
+                'idColonia'         => $informante->idColonia,
+                'idCodigoPostal'    => $informante->idColonia,
+            ]);
         
-        $cedula = array(
-            'entrevistadorNombres' => $carpeta->nombres,
-            'entrevistadorPrimerAp' => $carpeta->apellidos,
-            'entrevistadorSegundoAp' => '',
-            'entrevistadorCargo' => $carpeta->unidad,
-            'carpeta' => $carpeta->numCarpeta,
-            'idDialecto' => 1,
-            'entrevistadorPrimeraVez' => 'SI',
-        );
-        dd($cedula);*/
-
-        /*$persona = array(
-            'nombres' => ,
-            'primerAp' => ,
-            'segundoAp' => ,
-            'fechaNacimiento' => ,
-            'rfc' => ,
-            'curp' => ,
-            'sexo' => ,
-            'idEstadoOrigen' => ,
-            'idNacionalidad' => ,
-            'idMunicipioOrigen'
+        	\DB::commit();
+	        $data['success'] = true;
+        } catch (\Exception $e) {
+            $data['success'] = false;
+            $data['error'] = $e->getMessage();
+            \DB::rollback();
+            dd($data);
+        }
+        dd($data);
+        return redirect()->action(
+            'CedulaController@show', ['id' => $cedula->id]
         );
 
-        $interprete = array(
-            'apodo',
-            'edadAparente',
-            'edadExtravio',
-            'embarazo',
-            'tipoGestacion',
-            'numGestacion',
-            'rumoresBebe',
-            'pormenores',
-            'antecedentesJudiciales',
-            'organizacion',
-            'tipoPersona',
-            'otroDocIdentidad',
-            'numDocIdentidad',
-            'correoElectronico',
-            'telefonos',
-            'notificaciones',
-            'tieneHijos',
-            'notificaciones',
-            'informante',
-            'idPersona',
-            'idEdocivil',
-            'idOcupacion',
-            'idEscolaridad',
-            'idDialecto',
-            'idCargo',
-            'idParentesco',
-            'idDocumentoIdentidad',
-        );*/
+
     }
 }
