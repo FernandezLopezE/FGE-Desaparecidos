1e6f4168635f867d90cb7e80d82e9880d65dfd13
Carlos Avila Aguilar <krloz1003@gmail.com>
2018-05-31 21:50:29 -0500

    Estandarizando el nombre de los botones del modal de contacto e informante

--- resources/views/informante/modals/modal_informante.blade.php
+++ resources/views/informante/modals/modal_informante.blade.php
@@ -275,10 +275,9 @@
 		<!-- Fin del Contenido del formulario-->
 	  </div>
 	  <div class="modal-footer">
-	    <button type="button" class="btn btn-dark mr-auto" id="btnLimpiar"> LIMPIAR CAMPOS</button>
-	    <button type="button" class="btn btn-dark" id="btnEditarInformante" style="display:none">EDITAR</button>
-		<button type="button" class="btn btn-dark" id="btnGuardarInformante" >GUARDAR</button>
-		<button type="button" class="btn btn-danger" data-dismiss="modal">CERRAR</button>
+	    <button type="button" class="btn btn-dark" id="btnEditarInformante" style="display:none">Guardar</button>
+		<button type="button" class="btn btn-dark" id="btnGuardarInformante" >Guardar</button>
+		<button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
 	  </div>
 	</div>
   </div>
