e73f0b2ec1227719d4e19a3c46275b2de7b5012d
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-06-19 17:37:57 -0500

    Agregando consulta para generar oficio 1

--- app/Http/Controllers/OficioCedulaController.php
+++ app/Http/Controllers/OficioCedulaController.php
@@ -14,6 +14,38 @@ class OficioCedulaController extends Controller
     public function index()
     {
         //
+        $data = \DB::table('desaparecidos_cedula_investigacion AS dci')
+                    ->join('desaparecidos_personas AS dperson','dci.id','=','dperson.idCedula')
+                    ->join('persona AS person','person.id','=','dperson.idPersona')
+                    ->leftJoin('desaparecidos_domicilios AS domicilios', 'dperson.id', '=', 'domicilios.idDesaparecido')
+                    ->leftJoin('cat_colonia AS colonia', 'domicilios.idColonia', '=', 'colonia.id')
+                    ->leftJoin('cat_localidad AS localidad', 'domicilios.idLocalidad', '=', 'localidad.id')
+                    ->leftJoin('cat_municipio AS municipio', 'domicilios.idMunicipio', '=', 'municipio.id')
+                    ->leftJoin('cat_estado AS estado', 'domicilios.idEstado', '=', 'estado.id')
+                    ->leftJoin('cat_parentesco AS parentesco', 'dperson.idParentesco', '=', 'parentesco.id')
+                    ->leftJoin('cat_documento_identidad AS docIden', 'dperson.idDocumentoIdentidad', '=', 'docIden.id')
+                    ->select(\DB::raw('CONCAT(dci.entrevistadorNombres," ", dci.entrevistadorPrimerAp," ",dci.entrevistadorSegundoAp) AS entrevistador'),
+                        'dci.entrevistadorCargo AS cargo',
+                        'dci.carpeta AS numCarpeta',
+                        \DB::raw('DATE(dci.created_at) AS fechaRegistro'),
+                        'dci.desaparicionObservaciones as observacionDesa',
+                        \DB::raw('CONCAT(person.nombres," ",person.primerAp," ",person.segundoAp) AS informante'),
+                        'dperson.tipoPersona AS tipoPersona',
+                        'parentesco.nombre AS parentesco',
+                        'dperson.edadExtravio AS edadExtravio',
+                        'dperson.fotoDesaparecido AS foto',
+                        'domicilios.calle AS calle',
+                        'domicilios.numExterno AS numeroExt',
+                        'domicilios.numInterno AS numeroInt',
+                        'colonia.nombre AS colonia',
+                        'localidad.nombre AS localidad',
+                        'municipio.nombre AS municipio',
+                        'estado.nombre AS estado',
+                        'docIden.nombre AS documentoI'                            
+                        )
+                    ->get();
+
+        return response()->json( $this->json_oficio1());
     }
 
     /**
@@ -61,6 +93,44 @@ class OficioCedulaController extends Controller
         $pdf->setPaper('carta');
 
         return $pdf->stream('formato_'.time().'.pdf');
+    }
+
+
+    public function json_oficio1(){
+        $oficio1 = \DB::table('desaparecidos_cedula_investigacion AS dci')
+                    ->join('desaparecidos_personas AS dperson','dci.id','=','dperson.idCedula')
+                    ->join('persona AS person','person.id','=','dperson.idPersona')
+                    ->leftJoin('desaparecidos_domicilios AS domicilios', 'dperson.id', '=', 'domicilios.idDesaparecido')
+                    ->leftJoin('cat_colonia AS colonia', 'domicilios.idColonia', '=', 'colonia.id')
+                    ->leftJoin('cat_localidad AS localidad', 'domicilios.idLocalidad', '=', 'localidad.id')
+                    ->leftJoin('cat_municipio AS municipio', 'domicilios.idMunicipio', '=', 'municipio.id')
+                    ->leftJoin('cat_estado AS estado', 'domicilios.idEstado', '=', 'estado.id')
+                    ->leftJoin('cat_parentesco AS parentesco', 'dperson.idParentesco', '=', 'parentesco.id')
+                    ->leftJoin('cat_documento_identidad AS docIden', 'dperson.idDocumentoIdentidad', '=', 'docIden.id')
+                    ->select(\DB::raw('CONCAT(dci.entrevistadorNombres," ", dci.entrevistadorPrimerAp," ",dci.entrevistadorSegundoAp) AS entrevistador'),
+                        'dci.entrevistadorCargo AS cargo',
+                        'dci.carpeta AS numCarpeta',
+                        \DB::raw('DATE(dci.created_at) AS fechaRegistro'),
+                        'dci.desaparicionObservaciones as observacionDesa',
+                        \DB::raw('CONCAT(person.nombres," ",person.primerAp," ",person.segundoAp) AS informante'),
+                        'dperson.tipoPersona AS tipoPersona',
+                        'parentesco.nombre AS parentesco',
+                        'dperson.edadExtravio AS edadExtravio',
+                        'dperson.fotoDesaparecido AS foto',
+                        'domicilios.calle AS calle',
+                        'domicilios.numExterno AS numeroExt',
+                        'domicilios.numInterno AS numeroInt',
+                        'colonia.nombre AS colonia',
+                        'localidad.nombre AS localidad',
+                        'municipio.nombre AS municipio',
+                        'estado.nombre AS estado',
+                        'docIden.nombre AS documentoI'                            
+                        )
+                    ->where('dperson.idCedula',1)
+                    ->where('dperson.tipoPersona','INFORMANTE')
+                    ->get();
+
+        return $oficio1;
     }
 
     /**
