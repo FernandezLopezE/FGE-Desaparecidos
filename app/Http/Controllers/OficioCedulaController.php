d5ec87e275b28debcc391ecf2f612cb8e0588fe9
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-06-18 22:58:36 -0500

    Agregando ruta y creando controlador para la generación de oficio de la cédula del extraviado.

---
+++ app/Http/Controllers/OficioCedulaController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OficioCedulaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $datos = \App\Models\Desaparecido::where('idCedula', $id)
+                                            ->where('tipoPersona', 'DESAPARECIDA')
+                                            ->limit(1)
+                                            ->get();
+        
+        $desaparecido = \App\Models\Desaparecido::find($datos[0]->id);
+        //dd($desaparecido->partescuerpo->toArray());
+        $view = view('plantillas.cedulaMediaAfiliacion.oficioGeneral', compact('desaparecido'))->render();
+        $pdf =\App::make('dompdf.wrapper');
+        $pdf -> loadHTML($view);
+        
+        
+        $pdf->setPaper('carta');
+
+        return $pdf->stream('formato_'.time().'.pdf');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
