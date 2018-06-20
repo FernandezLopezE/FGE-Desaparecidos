109c102f47e0d55f2d25384780879d88c4f4061d
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-06-19 22:54:01 -0500

    Estableciendo plantilla general para los documentos

--- resources/views/plantillas/cedulaMediaAfiliacion/solicitud.blade.php
+++ resources/views/plantillas/cedulaMediaAfiliacion/solicitud.blade.php
@@ -7,37 +7,71 @@
 	<link rel="stylesheet" href="">
 </head>
 <body>
-	<center><p class="fuente">SOLICITUD DE PERSONAS EXTRAVIADAS, SUSTRAIDAS O AUSENTES</p></center>
-	<div id="emisor">
-		
-		<p ><strong class="fuente">LIC.</strong> <strong class="fuente">DAVID GARCÍA GALVÁN</strong></p>
-		<p > FISCAL ESPECIALIZADO EN ATENCIÓN DE DENUNCIAS POR PERSONAS DESAPARECIDAS.</p>
-		<p >ZONA CENTRO XALAPA.</p>	
-	</div>
+	<p style="text-align:center"><span style="font-size:12pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><strong><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">SOLICITUD DE PERSONAS EXTRAVIADAS, SUSTRAIDAS O AUSENTES</span></span></strong></span></span></p>
 
-	<div id="cuerpo">
-		<p>CON FUNDAMENTO EN EL ARTICULO 8° DE LA CONSTITUCIÓN POLÍTICA DE LOS ESTADOS UNIDOS MEXICANOS, LA QUE SUSCRIBE LUZ DEL ROSARIO MENDOZA GERÓN, CON DOMICILIO PARA OÍR Y RECIBIR TODO TIPO DE NOTIFICACIONES Y DOCUMENTOS EL UBICADO EN CALLE ÁLVARO OBREGÓN, SIN NÚMERO, COLONIA LOS PINOS, LOCALIDAD RINCONADA, MUNICIPIO DE EMILIANO ZAPATA, VERACRUZ, RESPETUOSAMENTE ME DIRIJO ANTE USTED PARA INFORMAR QUE MI PAREJA DE 25 AÑOS DE EDAD, DE QUIEN SE DESCONOCE SU PARADERO DESDE EL DÍA 11 DE JUNIO DE 2018, SIENDO LAS 15:00 APROXIMADAMENTE, RAZÓN POR LA CUAL ME PERMITO SOLICITAR DE LA MANERA MAS ATENTA, SE ELABORE LA CÉDULA DE IDENTIFICACIÓN CON LA IMAGEN Y MEDIA FILIACIÓN DE DICHA PERSONA Y SE DIFUNDA A NIVEL NACIONAL A TRAVÉS DE LAS FISCALÍAS Y/O PROCURADURÍAS GENERALES DE JUSTICIA DE LOS ESTADOS DE LA REPUBLICA, ASÍ COMO DE LAS SUBPROCURADURÍAS Y DE LAS DELEGACIONES ESTATALES DE LA PROCURADURÍA GENERAL DE LA REPUBLICA Y DE SU PAGINA DE INTERNET. PARA MAYOR CONOCIMIENTO, ANEXO LA SIGUIENTE DOCUMENTACIÓN EN COPIA SIMPLE:</p>
+<p style="text-align:justify">&nbsp;</p>
 
-		<p>
-			<table>
-				<caption>table title and/or explanatory text</caption>
-				<thead>
-					<tr>
-						<th>header</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th>ACTA DE NACIMIENTO DE LA PERSONA EXTRAVIADA</th>
-						<td>NO</td>
-					</tr>
-				</tbody>
-			</table>
-		</p>
-	</div>
-	
+<p style="text-align:justify">&nbsp;</p>
 
+<p style="text-align:justify"><span style="font-size:12pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><strong><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">LIC. DAVID GARCÍA GALVÁN</span></span></strong></span></span><br />
+<span style="font-size:12pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><strong><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">FISCAL ESPECIALIZADO EN ATENCIÓN DE</span></span></strong></span></span><br />
+<span style="font-size:12pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><strong><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">DENUNCIAS POR PERSONAS DESAPARECIDAS.</span></span></strong></span></span><br />
+<span style="font-size:12pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><strong><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">ZONA CENTRO XALAPA.</span></span></strong></span></span></p>
 
+<p style="text-align:justify">&nbsp;</p>
+
+<p style="text-align:justify">&nbsp;</p>
+
+<p><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">CON FUNDAMENTO EN EL ARTICULO 8° DE LA CONSTITUCIÓN POLÍTICA DE LOS ESTADOS UNIDOS MEXICANOS, LA QUE SUSCRIBE <strong>LUZ DEL ROSARIO MENDOZA GERÓN</strong>, CON DOMICILIO PARA OÍR Y RECIBIR TODO TIPO DE NOTIFICACIONES Y DOCUMENTOS EL UBICADO EN CALLE ÁLVARO OBREGÓN, SIN NÚMERO, COLONIA LOS PINOS, LOCALIDAD RINCONADA, MUNICIPIO DE EMILIANO ZAPATA, VERACRUZ, RESPETUOSAMENTE ME DIRIJO ANTE USTED PARA INFORMAR QUE MI PAREJA DE 25 AÑOS DE EDAD, DE QUIEN SE DESCONOCE SU PARADERO DESDE EL DÍA 11 DE JUNIO DE 2018, SIENDO LAS 15:00 APROXIMADAMENTE, RAZÓN POR LA CUAL <strong>ME PERMITO SOLICITAR DE LA MANERA MAS ATENTA, SE ELABORE LA CÉDULA DE IDENTIFICACIÓN CON LA IMAGEN Y MEDIA FILIACIÓN DE DICHA PERSONA Y SE DIFUNDA A NIVEL NACIONAL A TRAVÉS DE LAS FISCALÍAS Y/O PROCURADURÍAS GENERALES DE JUSTICIA DE LOS ESTADOS DE LA REPUBLICA, ASÍ COMO DE LAS SUBPROCURADURÍAS Y DE LAS DELEGACIONES ESTATALES DE LA PROCURADURÍA GENERAL DE LA REPUBLICA Y DE SU PAGINA DE INTERNET</strong>. PARA MAYOR CONOCIMIENTO, ANEXO LA SIGUIENTE DOCUMENTACIÓN EN COPIA SIMPLE:</span></span></p>
+
+<p>&nbsp;</p>
+
+<table align="center" border="1" cellpadding="1" cellspacing="1" style="width:500px">
+	<tbody>
+		<tr>
+			<td style="width:360px"><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">ACTA DE NACIMIENTO DE LA PERSONA EXTRAVIADA</span></span></td>
+			<td style="width:127px">&nbsp;</td>
+		</tr>
+		<tr>
+			<td style="width:360px"><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">CARPETA DE INVESTIGACIÓN O CONSTANCIA DE HECHOS</span></span></td>
+			<td style="width:127px">&nbsp;</td>
+		</tr>
+		<tr>
+			<td style="width:360px"><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">FOTOGRAFÍA DE LA PERSONA EXTRAVIADA</span></span></td>
+			<td style="width:127px">&nbsp;</td>
+		</tr>
+		<tr>
+			<td style="width:360px"><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">IDENTIFICACIÓN OFICIAL DEL SUSCRITO</span></span></td>
+			<td style="width:127px">&nbsp;</td>
+		</tr>
+		<tr>
+			<td style="width:360px"><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">RESOLUCIÓN JUDICIAL QUE CONCEDA LA GUARDA CUSTODIA PROVISIONAL O DEFINITIVA</span></span></td>
+			<td style="width:127px">&nbsp;</td>
+		</tr>
+	</tbody>
+</table>
+
+<p style="text-align:justify"><span style="font-size:12pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">EN CASO DE CAMBIO DE DOMICILIO Y/O NÚMEROS TELEFÓNICOS, Y CUALQUIER DATO PROPORCIONADO SOBRE EL PARADERO Y LOCALIZACIÓN DE MI FAMILIAR EXTRAVIADO, ME COMPROMETO A INFORMARLE A LA BREVEDAD, LO ANTERIOR, A FIN DE HACER LA CANCELACIÓN DE LA CEDULA DE DIFUSIÓN CORRESPONDIENTE. DECLARO BAJO PROTESTA DE DECIR VERDAD QUE TODA LA INFORMACIÓN ARRIBA ASENTADA ES CIERTA.</span></span></span></span></p>
+
+<p style="text-align:justify">&nbsp;</p>
+
+<p style="text-align:justify"><span style="font-size:12pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">AGRADECIENDO DE ANTEMANO LA ATENCIÓN QUE LE SIRVA PRESENTAR A LA PRESENTE, REITERO A USTED MI CONSIDERACIÓN MAS DISTINGUIDA.</span></span></span></span></p>
+
+<p style="text-align:justify">&nbsp;</p>
+
+<p style="text-align:justify">&nbsp;</p>
+
+<p style="text-align:center"><span style="font-size:12pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">NOMBRE DE LA PETICIONARIA</span></span></span></span></p>
+
+<p style="text-align:center">&nbsp;</p>
+
+<p style="text-align:center">&nbsp;</p>
+
+<p style="text-align:center">&nbsp;</p>
+
+<p style="text-align:center"><strong><span style="font-size:11.0pt"><span style="font-family:&quot;Tahoma&quot;,&quot;sans-serif&quot;">LUZ DEL ROSARIO MENDOZA GERÓN</span></span></strong></p>
+
+<p>&nbsp;</p>
 
 
 </body>
