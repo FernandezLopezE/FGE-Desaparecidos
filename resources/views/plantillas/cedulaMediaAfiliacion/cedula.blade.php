c11fde47f1092681c6fd7b0bc9e96dbd70729057
ADFGE\wburgos <wburgos@fiscaliaveracruz.gob.mx>
2018-06-21 17:07:30 -0500

    Muestra tabla correcta en pdf

--- resources/views/plantillas/cedulaMediaAfiliacion/cedula.blade.php
+++ resources/views/plantillas/cedulaMediaAfiliacion/cedula.blade.php
@@ -5,80 +5,293 @@
 	<title>Document</title>
 </head>
 <body>
-	<table style="height: 774px; width: 716px;" border="1" cellspacing="0" cellpadding="0" align="center">
-<tbody>
-<tr>
-<td style="background-color: #d9d9d9; border-color: black; height: 1px; text-align: center; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;"><strong>CEDULA DE PERSONAS EXTRAVIADAS, SUSTRA&Iacute;DAS O AUSENTES</strong></span></span></td>
-</tr>
-<tr>
-<td style="background-color: #d9d9d9; border-color: black; height: 1px; text-align: center; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;"><strong>C&Eacute;DULA DE RECEPCI&Oacute;N</strong></span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 1px; text-align: center; vertical-align: top; width: 185px;" colspan="2.5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">CARPETA DE INVESTIGACI&Oacute;N: </span></span><br /> <span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;"><strong>FEADPD/ZCX/61/2018-06</strong></span></span></td>
-<td style="height: 1px; text-align: center; vertical-align: top; width: 230px;" colspan="2.5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">FECHA Y &nbsp;HORA &nbsp;DE REGISTRO:</span></span><br /> <span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">12 DE JUNIO DE 2018 </span></span><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">A LAS 11:30 HORAS</span></span></td>
-<td style="border-color: black; height: 1px; text-align: center; vertical-align: top; width: 291px;" colspan="2.5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">TIPO DE SOLICITUD: </span></span><br /> <span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">PERSONAL (<strong>X</strong>) CORRESPONDENCIA ( )&nbsp;&nbsp; TELEF&Oacute;NICA ( )</span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 5.25pt; text-align: center; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">FISCAL ESPECIALIZADO EN LA ATENCI&Oacute;N DE DENUNCIAS POR PERSONAS DESAPARECIDAS, ZONA CENTRO XALAPA.</span></span></td>
-</tr>
-<tr>
-<td style="background-color: #d9d9d9; border-color: black; height: 6.85pt; text-align: center; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;"><strong>DATOS PERSONALES</strong></span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 6.85pt; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">NOMBRE: <strong>CESAR ARTUTO BRAVO CAMPOS</strong></span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 4.95pt; vertical-align: top; width: 185px;" colspan="2"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">EDAD: 25 a&ntilde;os, 6 meses y 18 dias</span></span></td>
-<td style="height: 4.95pt; vertical-align: top; width: 230px;" colspan="2"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">G&Eacute;NERO:</span></span></td>
-<td style="height: 4.95pt; vertical-align: top; width: 291px;"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">EDO. CIVIL:</span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 4.1pt; text-align: center; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">DOMICILIO:</span></span><br /> <span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">CALLE: &Aacute;LVARO OBREG&Oacute;N, NUMERO: S/N, COLONIA:&nbsp; LOS PINOS, LOCALIDAD: RINCONADA, MUNICIPIO:&nbsp; EMILIANO ZAPATA, ENTIDAD: VERACRUZ, C.P.:</span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 3.9pt; vertical-align: top; width: 419px;" colspan="4"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">ESCOLARIDAD: TERCERO DE PRIMARIA</span></span></td>
-<td style="height: 3.9pt; vertical-align: top; width: 291px;"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">OCUPACI&Oacute;N: OBRERO</span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 4.7pt; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">EXTRAVIADO (&nbsp;&nbsp; )&nbsp;&nbsp;&nbsp; SUSTRACCI&Oacute;N&nbsp; (&nbsp; X )&nbsp;&nbsp; AUSENCIA ( &nbsp;)&nbsp; </span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 5.25pt; text-align: center; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">LUGAR DE EXTRAVI&Oacute;: CARRETERA XALAPA, VERACRUZ, LOCALIDAD RINCONADA</span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 5.1pt; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">EN CASO DE CONTROVERSIA FAMILIAR SI SE LO LLEVO:</span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 5.4pt; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">PAPA:----------</span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 5.25pt; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">MAMA:--------</span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 5.25pt; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">OTRO FAMILIAR:--------- .</span></span></td>
-</tr>
-<tr>
-<td style="background-color: #d9d9d9; border-color: black; height: 6.35pt; text-align: center; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;"><strong>DATOS DE LA PERSONA SOLICITANTE</strong></span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 5.65pt; vertical-align: top; width: 185px;" colspan="2"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">NOMBRE: LUZ DEL ROSARIO MENDOZA GER&Oacute;N</span></span></td>
-<td style="width: 506px;" colspan="3"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">PARENTESCO CON EL DESAPARECIDO: PAREJA</span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 6.45pt; vertical-align: top; width: 185px;" colspan="2"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">EDAD: 25 a&ntilde;os, 6 meses y 18 dias</span></span></td>
-<td style="height: 6.45pt; vertical-align: top; width: 230px;" colspan="2"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">G&Eacute;NERO:</span></span></td>
-<td style="height: 6.45pt; vertical-align: top; width: 291px;"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">EDO. CIVIL:</span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 14px; text-align: center; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">DOMICILIO:</span></span><br /> <span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;">CALLE: &Aacute;LVARO OBREG&Oacute;N, NUMERO: S/N, COLONIA:&nbsp; LOS PINOS, LOCALIDAD: RINCONADA, MUNICIPIO:&nbsp; EMILIANO ZAPATA, ENTIDAD: VERACRUZ, C.P.:</span></span></td>
-</tr>
-<tr>
-<td style="background-color: #d9d9d9; border-color: black; height: 6.15pt; text-align: center; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;"><strong>S&Iacute;NTESIS</strong></span></span></td>
-</tr>
-<tr>
-<td style="border-color: black; height: 4px; vertical-align: top; width: 449px;" colspan="5"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;"><strong>REFIERE LA DENUNCIANTE QUE EL D&Iacute;A DE AYER 11 DE JUNIO DE 2018, SIENDO LAS 15:00 APROXIMADAMENTE, AL ENCONTRARSE TRABAJANDO EN LAS AFUERAS DE LA TIENDA TRES HERMANOS, LLEGARON TRES CAMIONETAS CON VARIOS SUJETOS Y SE LLEVARON AL AHORA DESAPARECIDO, CON RUMBO DESCONOCIDO. </strong></span></span></td>
-</tr>
-</tbody>
+<table border="1" align="center">
+	<tbody>
+		<tr>
+			<td style="background-color: #d9d9d9; border-color: black; text-align: center;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						<strong>CEDULA DE PERSONAS EXTRAVIADAS, SUSTRA&Iacute;DAS O AUSENTES</strong>
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="background-color: #d9d9d9; border-color: black; text-align: center;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						<strong>C&Eacute;DULA DE RECEPCI&Oacute;N</strong>
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; text-align: center; vertical-align: top; " colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						CARPETA DE INVESTIGACI&Oacute;N: 
+					</span>
+				</span>
+				<br />
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						<strong>FEADPD/ZCX/61/2018-06</strong>
+					</span>
+				</span>
+			</td>
+			<td style="border-color: black; height: 1px; text-align: center; vertical-align: top; width: 185px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						FECHA Y &nbsp;HORA &nbsp;DE REGISTRO:
+					</span>
+				</span>
+				<br />
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						12 DE JUNIO DE 2018
+					</span>
+				</span>
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						A LAS 11:30 HORAS
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 1px; text-align: center; vertical-align: top; width: 291px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						TIPO DE SOLICITUD: 
+					</span>
+				</span>
+				<br />
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						PERSONAL (<strong>X</strong>) CORRESPONDENCIA ( )&nbsp;&nbsp; TELEF&Oacute;NICA ( )
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 5.25pt; text-align: center; vertical-align: top; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						FISCAL ESPECIALIZADO EN LA ATENCI&Oacute;N DE DENUNCIAS POR PERSONAS DESAPARECIDAS, ZONA CENTRO XALAPA.
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="background-color: #d9d9d9; border-color: black; height: 6.85pt; text-align: center; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						<strong>DATOS PERSONALES</strong>
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 6.85pt; vertical-align: top; width: 449px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						NOMBRE: <strong>CESAR ARTUTO BRAVO CAMPOS</strong>
+					</span>
+				</span>
+			</td>
+			<td style="border-color: black; height: 4.95pt; vertical-align: top; width: 185px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						EDAD: 25 a&ntilde;os, 6 meses y 18 dias
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="height: 4.95pt; vertical-align: top; width: 230px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						G&Eacute;NERO:
+					</span>
+				</span>
+			</td>
+			<td style="height: 4.95pt; vertical-align: top; width: 291px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						EDO. CIVIL:
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 4.1pt; text-align: center; vertical-align: top; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						DOMICILIO:
+					</span>
+				</span>
+				<br />
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						CALLE: &Aacute;LVARO OBREG&Oacute;N, NUMERO: S/N, COLONIA:&nbsp; LOS PINOS, LOCALIDAD: RINCONADA, MUNICIPIO:&nbsp; EMILIANO ZAPATA, ENTIDAD: VERACRUZ, C.P.:
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 3.9pt; vertical-align: top; width: 419px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						ESCOLARIDAD: TERCERO DE PRIMARIA
+					</span>
+				</span>
+			</td>
+			<td style="height: 3.9pt; vertical-align: top; width: 291px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						OCUPACI&Oacute;N: OBRERO
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 4.7pt; vertical-align: top; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						EXTRAVIADO (&nbsp;&nbsp; )&nbsp;&nbsp;&nbsp; SUSTRACCI&Oacute;N&nbsp; (&nbsp; X )&nbsp;&nbsp; AUSENCIA ( &nbsp;)&nbsp; 
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 5.25pt; text-align: center; vertical-align: top; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						LUGAR DE EXTRAVI&Oacute;: CARRETERA XALAPA, VERACRUZ, LOCALIDAD RINCONADA
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 5.1pt; vertical-align: top; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						EN CASO DE CONTROVERSIA FAMILIAR SI SE LO LLEVO:
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 5.4pt; vertical-align: top; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						PAPA:----------
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 5.25pt; vertical-align: top; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						MAMA:--------
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 5.25pt; vertical-align: top; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						OTRO FAMILIAR:--------- 
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="background-color: #d9d9d9; border-color: black; height: 6.35pt; text-align: center; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						<strong>DATOS DE LA PERSONA SOLICITANTE</strong>
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 5.65pt; vertical-align: top; width: 185px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						NOMBRE: LUZ DEL ROSARIO MENDOZA GER&Oacute;N
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 6.45pt; vertical-align: top; width: 185px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						EDAD: 25 a&ntilde;os, 6 meses y 18 dias
+					</span>
+				</span>
+			</td>
+			<td style="border-color: black; height: 6.45pt; vertical-align: top; width: 185px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						PARENTESCO CON EL DESAPARECIDO: PAREJA
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="height: 6.45pt; vertical-align: top; width: 230px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						G&Eacute;NERO:
+					</span>
+				</span>
+			</td>
+			<td style="height: 6.45pt; vertical-align: top; width: 291px;" colspan="1">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						EDO. CIVIL:
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 14px; text-align: center; vertical-align: top; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						DOMICILIO:
+					</span>
+				</span>
+				<br />
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						CALLE: &Aacute;LVARO OBREG&Oacute;N, NUMERO: S/N, COLONIA:&nbsp; LOS PINOS, LOCALIDAD: RINCONADA, MUNICIPIO:&nbsp; EMILIANO ZAPATA, ENTIDAD: VERACRUZ, C.P.:
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="background-color: #d9d9d9; border-color: black; height: 6.15pt; text-align: center; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						<strong>S&Iacute;NTESIS</strong>
+					</span>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-color: black; height: 4px; vertical-align: top; width: 449px;" colspan="2">
+				<span style="font-size: 11px;">
+					<span style="font-family: Tahoma,Geneva,sans-serif;">
+						<strong>REFIERE LA DENUNCIANTE QUE EL D&Iacute;A DE AYER 11 DE JUNIO DE 2018, SIENDO LAS 15:00 APROXIMADAMENTE, AL ENCONTRARSE TRABAJANDO EN LAS AFUERAS DE LA TIENDA TRES HERMANOS, LLEGARON TRES CAMIONETAS CON VARIOS SUJETOS Y SE LLEVARON AL AHORA DESAPARECIDO, CON RUMBO DESCONOCIDO. </strong>
+					</span>
+				</span>
+			</td>
+		</tr>
+	</tbody>
 </table>
 <p style="text-align: center;"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;"><strong>A T E N T A M E N T E</strong></span></span></p>
 <p style="text-align: center;"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;"><strong>FISCAL ESPECIALIZADO PARA LA ATENCI&Oacute;N DE DENUNCIAS</strong><br /> <strong>POR PERSONAS DESAPARECIDAS, ZONA CENTRO XALAPA</strong></span></span></p>
@@ -86,6 +299,5 @@
 <p style="text-align: center;">&nbsp;</p>
 <p style="text-align: center;">&nbsp;</p>
 <p style="text-align: center;"><span style="font-size: 11px;"><span style="font-family: Tahoma,Geneva,sans-serif;"><strong>LIC. DAVID GARC&Iacute;A GALV&Aacute;N</strong></span></span></p>
-<p>&nbsp;</p>
 </body>
 </html>
