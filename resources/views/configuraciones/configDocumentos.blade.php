591a50f9d8fa272fe6d411675ced0ad4b0caf027
ADFGE\dlanda <proyectosmx94@gmail.com>
2018-06-20 19:55:10 -0500

    Creacion de modales

--- resources/views/configuraciones/configDocumentos.blade.php
+++ resources/views/configuraciones/configDocumentos.blade.php
@@ -7,59 +7,65 @@
   <div class="form-group row">
     <div class="col">
       <label for="">Oficios </label>
-      {!! Form::select('size', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SI' => 'SI', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'atencionOdonto'] ) !!}
+      <button class="btn btn-primary btn-sm pull-right" id="#" data-toggle="tooltip" data-placement="top" title="Nuevo oficio"><i class="fa fa-plus-circle"></i></button>
+      {!! Form::select('oficios', $oficios, '', ['class' => 'form-control', 'id' => 'catOficios'] ) !!}
     </div>
       
     <div class="col">
       <label for="">Dependencias</label>
-      <button class="btn btn-primary btn-sm pull-right" id="newDepen" data-toggle="tooltip" data-placement="left" title="Nueva dependencia"><i class="fa fa-plus-circle"></i></button>
-      {!! Form::select('size', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SI' => 'SI', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'atencionOdonto'] ) !!}
+      <button class="btn btn-primary btn-sm pull-right" id="newDepen" data-toggle="tooltip" data-placement="top" title="Nueva dependencia"><i class="fa fa-plus-circle"></i></button>
+      {!! Form::select('dependencias', $dependencias, '', ['class' => 'form-control', 'id' => 'catDependencias'] ) !!}
     </div>
+    @include('includes.configuraciones.modal_ConfigDepend')
 
     <div class="col">
       <label for="">Encargado</label>
-      <button class="btn btn-primary btn-sm pull-right" id="newEncar" data-toggle="tooltip" data-placement="left" title="Nuevo encargado"><i class="fa fa-plus-circle"></i></button>
-      {!! Form::select('size', array('SIN INFORMACIÓN' => 'SIN INFORMACIÓN', 'SI' => 'SI', 'NO' => 'NO'), '', ['class' => 'form-control', 'id' => 'atencionOdonto'] ) !!}
+      <button class="btn btn-primary btn-sm pull-right" id="newEncar" data-toggle="tooltip" data-placement="top" title="Nuevo encargado"><i class="fa fa-plus-circle"></i></button>
+      {!! Form::select('encargados', $encargados, '', ['class' => 'form-control', 'id' => 'catEncargados'] ) !!}
     </div>
+    @include('includes.configuraciones.modal_ConfigEncargado')
 
     <div class="col-1">
-      <label for="">.</label>
+      <label for=""><font color="#FFFFFF">.</font></label>
       <button class="form-control btn-primary">Agregar</button>
     </div>
-  </div>
+  </div><br>
+
+  <table class="table table-bordered table-hover">
+    <thead>
+      <tr>
+        <th>Oficio</th>
+        <th>Dependencia</th>
+        <th>Encargado</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>prueba</th>
+        <th>prueba</th>
+        <th>prueba</th>
+        <th>prueba</th>
+      </tr>
+    </tbody>
+</table>
   
 </div>
 
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+$(document).ready(function(){
+  $("#newDepen").click(function(event) {
+   $("#modal_ConfigDepend").modal();
+  });
+
+  $("#newEncar").click(function(event) {
+   $("#modal_ConfigEncargado").modal();
+  });
 
 
-<div class="row">
-  <div class="col">
-    <h5>SECRETARIA DE SEGURIDAD PÚBLICA</h5>
-    <label for="">Dirigido a</label>
-  </div>
-</div>
-
-<div class="form-group row">
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Nombre">
-  </div>
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Primer apellido">
-  </div>
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Segundo apellido">
-  </div>
-</div>
-
-<div class="form-group row">
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Cargo que desempeña">
-  </div>
-</div>
-
-<div class="form-group row">
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Correo eléctronico (opcional)">
-  </div>
-</div>
+});
+</script>
 @endsection
