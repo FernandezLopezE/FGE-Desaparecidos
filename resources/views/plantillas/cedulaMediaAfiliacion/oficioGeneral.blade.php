7304109701c3b39bacd48d7b8215c2c9667ac032
Ruben8A <ochoa.rrub@gmail.com>
2018-06-21 11:36:38 -0500

    agregando div y saltos de pagina para la generacion del pdf

--- resources/views/plantillas/cedulaMediaAfiliacion/oficioGeneral.blade.php
+++ resources/views/plantillas/cedulaMediaAfiliacion/oficioGeneral.blade.php
@@ -4,8 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
-	<meta name="csrf-token" content="{{csrf_token()}}">
-	<link rel="stylesheet"  href="{{asset ('css/oficio.css')}}">
+	<!-- <meta name="csrf-token" content="{{csrf_token()}}">
+	<link rel="stylesheet"  href="{{asset ('css/oficio.css')}}"> -->
 	<style>
 	.page-break {
 	    page-break-after: always;
@@ -17,15 +17,19 @@
 	</style>
 </head>
 <body>
+	<div class="page-break">@include('plantillas.cedulaMediaAfiliacion.solicitud')</div>
+	<div class="page-break">@include('plantillas.cedulaMediaAfiliacion.cedula')</div>
+	<div class="page-break">pagina 3</div>
+	<div class="page-break">pagina 4</div>
+	<div class="page-break">@include('desaparecidos.boletin')</div>
 
-
-	<div id="app">
+	<!-- <div id="app">
 		<oficio tipo="solicitud de la persona extraviada" url="{{url("jsonOficio1")}}" id="1" ></oficio>
 		<oficio tipo="cedula persona extraviada" url="{{url("json_oficio2")}}" id="1" ></oficio>
 		
-	</div>
+	</div> -->
 	
 </body>
 </html>
-<script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
-<script src="{{asset('js/oficio.js')}}"></script>
+<!-- <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('js/oficio.js')}}"></script> -->
